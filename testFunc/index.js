d9573e1fcd7a49f17a5c43a8e796657565ec6835
waltermitch <devmitch256@gmail.com>
2021-04-30 17:39:34 -0700

    made config a writable variable

--- testFunc/index.js
+++ testFunc/index.js
@@ -1,3 +1,10 @@
 const start = require('../tools/start');
 
-module.exports = async (context) => App.next(context, start);
+module.exports = async (context) => App.next(context, get);
+
+async function get(context)
+{
+    start();
+
+    context.log(config);
+}
