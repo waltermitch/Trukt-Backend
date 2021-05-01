42108956595efdbca7381ad78d00a6889806ed06
waltermitch <devmitch256@gmail.com>
2021-04-30 17:33:04 -0700

    removed useless trycatch

--- testFunc/index.js
+++ testFunc/index.js
@@ -1,32 +1,3 @@
-const ErrorHandler = require('../Classes/ErrorHandler');
 const start = require('../tools/start');
 
-module.exports = async function (context, req)
-{
-    let response = { status: 400, data: 'Missing Payload' };
-
-    if (req?.body)
-    
-        try
-        {
-            response = await test(context, req);
-        }
-        catch
-        {
-            response = new ErrorHandler(context, req);
-        }
-
-    context.res =
-    {
-        headers: { 'Content-Type': 'application/json' },
-        status: response.status,
-        body: response
-    };
-};
-
-async function test()
-{
-    start();
-
-    return { 'status': 200 };
-}
+module.exports = async (context) => App.next(context, start);
