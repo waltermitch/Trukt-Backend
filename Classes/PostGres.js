6fb512dd09edf3559212fb6c76a5dfb10c1548e4
waltermitch <devmitch256@gmail.com>
2021-05-10 15:02:40 -0700

    added pg connect method

--- Classes/PostGres.js
+++ Classes/PostGres.js
@@ -1,5 +1,9 @@
-// const knex = require('knex');
-// const pg = require('pg');
+const urlParser = require('pg-connection-string').parse;
+const Heroku = require('./HerokuPlatformAPI');
+const knex = require('knex');
+const pg = require('pg');
+
+let db;
 
 class PG
 {
@@ -8,7 +12,23 @@ class PG
 
     static async connect()
     {
+        if(!db)
+        {
+            // get url
+            const res = await Heroku.getConfig();
 
+            // parse url
+            const config = urlParser(res.DATABASE_URL);
+
+            // connect
+            db = knex(
+                {
+                    client: 'pg',
+                    connection: Object.assign({ ssl: { rejectUnauthorized: false }}, config)
+                }).withSchema('salesforce');
+        }
+
+        return db;
     }
 }
 
