ae58b2594a09dafa512699b0fab7fcec584fef18
waltermitch <devmitch256@gmail.com>
2021-05-19 14:00:19 -0700

    fixed config.enc conflic STS-1318

--- getAccount/index.js
+++ getAccount/index.js
@@ -2,5 +2,5 @@ module.exports = async (context, req) => await App.next(context, getAccount, req
 
 async function getAccount(context, req)
 {
-    context.log(req.body);
+    context.log(req.params);
 }
