8908617ee5a285d5159004a1141b406ca287c04b
waltermitch <devmitch256@gmail.com>
2021-06-03 10:24:26 -0700

    [STS-1311] added order number trigger function to automatically assign order numbers

---
+++ migrations/20210101000001_order_number_assign_trigger_func.js
@@ -0,0 +1,37 @@
+const db_owner = require('../tools/migration').db_owner;
+
+const function_name = 'rcg_order_number_assign';
+
+exports.up = function (knex)
+{
+    return knex.raw(`
+    CREATE FUNCTION rcg_tms.${function_name}()
+        RETURNS trigger
+        LANGUAGE 'plpgsql'
+        COST 100
+        STABLE NOT LEAKPROOF
+    AS $BODY$
+    BEGIN
+        IF (TG_OP = 'INSERT') THEN
+            NEW.number = rcg_tms.rcg_next_order_number();
+        ELSEIF (TG_OP = 'UPDATE') THEN
+        -- do not allow users to change the guid once it is assigned
+            IF (NEW.number <> OLD.number ) THEN
+                NEW.number = OLD.number;
+            END IF;
+        END IF;
+        RETURN NEW;
+    END;
+    $BODY$;
+
+    ALTER FUNCTION rcg_tms.${function_name}()
+        OWNER TO ${db_owner};
+
+    COMMENT ON FUNCTION rcg_tms.${function_name}()
+        IS 'Assigns the order the next available order number and prevents changing it';`);
+};
+
+exports.down = function (knex)
+{
+    return knex.raw(`DROP FUNCTION rcg_tms.${function_name}();`);
+};
