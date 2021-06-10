24995259c034dfb94228690a3cfd170e69ea6d7a
waltermitch <devmitch256@gmail.com>
2021-06-09 17:45:48 -0700

    [STS-1311] added timestamps and binary columns to invoice bill table

--- migrations/20210525003148_invoice_bill.js
+++ migrations/20210525003148_invoice_bill.js
@@ -14,16 +14,19 @@ exports.up = function (knex)
         migration_tools.timestamps(knex, table);
         table.datetime('date_filed');
         table.datetime('date_due');
+        table.datetime('date_paid');
 
         table.decimal('total', 15, 2).comment('This is calculated, please do not modify manually');
 
         table.string('payment_method');
         table.string('payment_terms');
 
+        table.binary('is_valid').notNullable().defaultTo(false).comment('the invoice or bill is valid when this is set, by default invalid, when user "generates" then sets it to valid');
+        table.binary('is_generated').notNullable().defaultTo(false).comment('This is set to true when a user generates the invoice or bill in the system');
         table.binary('is_paid').notNullable().defaultTo(false);
         table.binary('is_invoice').notNullable().comment('true value is invoice, false value is bill');
 
-    }).raw(migration_tools.guid_function(table_name));
+    }).raw(migration_tools.guid_function(table_name)).raw(migration_tools.timestamps_trigger(table_name));
 };
 
 exports.down = function (knex)
