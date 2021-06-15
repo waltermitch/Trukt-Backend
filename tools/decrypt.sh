c5b1bcc964af6eda31b5988a0ba24daf054cf445
waltermitch <devmitch256@gmail.com>
2021-06-15 14:03:35 -0700

    added error message for env vars not being set

--- tools/decrypt.sh
+++ tools/decrypt.sh
@@ -1,1 +1,10 @@
-openssl enc -d -aes-256-cbc -md sha512 -S $ENC_SALT -K $ENC_KEY -iv $ENC_IV -in $1 -out $2
+CONTACT_EMAIL='vklimkiv@rcglogistics.com'
+
+if [[ -z $ENC_SALT ]] || [[ -z $ENC_KEY ]] || [[ -z $ENC_IV ]]; then
+    echo "ERROR:"
+    echo "ENC_SALT, ENC_KEY, ENC_IV environment vars not set."
+    echo "Contact $CONTACT_EMAIL"
+    exit 1
+else
+    openssl enc -d -aes-256-cbc -md sha512 -S $ENC_SALT -K $ENC_KEY -iv $ENC_IV -in $1 -out $2
+fi
