51413c3c082d5baae532a0a2faea8394547c2c01
waltermitch <devmitch256@gmail.com>
2021-06-15 14:57:46 -0700

    BaseModel to be used for all Models

--- Classes/Models/BaseModel.js
+++ Classes/Models/BaseModel.js
@@ -1,4 +1,10 @@
 const { Model } = require('objection');
+const Knex = require('knex');
+const knexfile = require('../../knexfile');
+
+const knex = Knex(knexfile());
+
+Model.knex(knex);
 
 class BaseModel extends Model
 {
