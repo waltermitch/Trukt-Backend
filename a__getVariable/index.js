224cdbdcf103e4c4b874b3eb53cc3e83dc86ee24
waltermitch <devmitch256@gmail.com>
2021-06-16 12:22:19 -0700

    [STS-1311] renamed all the function app folder names

--- a__getVariable/index.js
+++ a__getVariable/index.js
@@ -1,4 +1,5 @@
 const PG = require('../Classes/PostGres');
+const App = require('../Classes/HttpControllers/HttpRouteController');
 
 module.exports = async (context, req) => App.next(context, getVar, req);
 
