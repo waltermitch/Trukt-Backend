9937df9b5c01dde94bd7b4d818db70ba57e12081
waltermitch <devmitch256@gmail.com>
2021-06-18 16:58:41 -0700

    changed account controller to do some minor validation

--- Classes/HttpControllers/AccountController.js
+++ Classes/HttpControllers/AccountController.js
@@ -1,14 +1,45 @@
 const HttpRouteController = require('./HttpRouteController');
 const AccountService = require('../Services/AccountService');
+const { uuidRegex } = require('../Utils/Regexes');
 
-class SearchAccountsController extends HttpRouteController
+class AccountController extends HttpRouteController
 {
     async handleGet(context, req)
     {
-        return await AccountService.searchByType(req.params.type, req.params.query);
+        const res = {};
+        if (!('accountId' in req.params) && this.searchQueryCriteria(req.query))
+        {
+            res.body = await AccountService.searchByType(req.params.accountType, req.query.search);
+        }
+        else if (('accountId' in req.params) && uuidRegex.test(req.params.accountId))
+        {
+            const result = await AccountService.searchByType(req.params.accountType, req.params.accountId);
+            if (result.length > 0)
+            {
+                res.status = 200;
+                res.body = result[0];
+            }
+            else
+            {
+                res.status = 404;
+            }
+        }
+        else
+        {
+            res.status = 400;
+        }
+        return res;
+    }
+
+    searchQueryCriteria(query)
+    {
+        const clone = Object.assign({}, query);
+        delete clone.pg;
+        delete clone.rc;
+        return Object.keys(clone).length > 0;
     }
 }
 
-const controller = new SearchAccountsController();
+const controller = new AccountController();
 
 module.exports = controller;
