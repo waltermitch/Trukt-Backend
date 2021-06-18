4ae8b704f4f3d51881d39919fd589ff1bf6d8817
waltermitch <devmitch256@gmail.com>
2021-06-17 22:24:40 -0700

    [STS-1410] added requirements for searching and fixed objection response with contacts

--- Classes/Services/TerminalService.js
+++ Classes/Services/TerminalService.js
@@ -1,31 +1,105 @@
 const Terminal = require('../Models/Terminal');
 
-const searchFields = [
-    'name',
-    'street1',
-    'street2',
-    'city',
-    'zipCode',
-    'state'
-];
+const keywordFields = {
+    'name': ['name'],
+    'country': ['country'],
+    'state': ['state'],
+    'city': ['city'],
+    'zip': ['zipCode'],
+    'address': ['street1', 'street2']
+};
 
 class TerminalService
 {
     static async getById(terminalId)
     {
-        const terminal = await Terminal.query().where('rcg_tms.terminals.guid', '=', `${terminalId}`).withGraphJoined('[contacts, primaryContact, alternativeContact]');
+        const terminal = await Terminal.query().where('rcgTms.terminals.guid', '=', `${terminalId}`).withGraphJoined('[contacts, primaryContact, alternativeContact]');
         return terminal;
     }
 
-    static async search(keyword)
+    static async search(query)
     {
-        const terminal = await Terminal.query().withGraphJoined('[contacts, primaryContact, alternativeContact]').orWhere(builder =>
-        {
-            for (const field of searchFields)
-                builder.orWhere(field, 'ilike', `%${keyword}%`);
+        for (const key in query)
 
-        });
-        return terminal;
+            // remove unwanted keys that undefined or empty strings
+            if (query[key] == undefined || query[key] == '')
+
+                delete query[key];
+
+        // query will be an object with key being the query word
+        // min 1 page, with default page 1 (1st page)
+        // objection is 0 index, so is postgress, user input will be starting page index at 1
+        const pg = Math.max(1, query.pg || 1) - 1;
+
+        // clamp between 0 and 100 with default value of 10
+        const rc = Math.min(100, Math.max(1, query.rc || 10));
+
+        let qb = Terminal.query();
+        let orderbypriority = [];
+        const normalOnes = [];
+        const searchOnes = [];
+
+        for (const keyword in keywordFields)
+
+            if (keyword in query)
+
+                for (const field of keywordFields[keyword])
+                {
+                    orderbypriority.push(field);
+                    normalOnes.push({ field: field, query: query[keyword] });
+                }
+
+        if ('search' in query)
+
+            for (const keyword in keywordFields)
+
+                if (!(keyword in query))
+
+                    for (const field of keywordFields[keyword])
+                    {
+                        orderbypriority.push(field);
+                        searchOnes.push({ field: field, query: query.search });
+                    }
+
+        if (normalOnes.length > 0)
+
+            qb = qb.where((builder) =>
+            {
+                for (const o of normalOnes)
+                {
+                    const q = o.query.replace(/%/g, '');
+                    builder.orWhere(`${o.field}`, 'ilike', `%${q}%`);
+                }
+            });
+
+        if (searchOnes.length > 0)
+
+            qb = qb.where((builder) =>
+            {
+                for (const o of searchOnes)
+                {
+                    const q = o.query.replace(/%/g, '');
+                    builder.orWhere(`${o.field}`, 'ilike', `%${q}%`);
+                }
+            });
+
+        if ('lat' in query)
+        {
+            orderbypriority.push('latitude');
+            qb = qb.where('latitude', query.lat);
+        }
+        if ('long' in query)
+        {
+            orderbypriority.push('longitude');
+            qb = qb.where('longitude', query.long);
+        }
+
+        orderbypriority = orderbypriority.map((x) => { return { column: `rcgTms.terminals.${x}`, order: 'asc' }; });
+
+        qb = qb.orderBy(orderbypriority).page(pg, rc);
+        const terminal = await qb.withGraphFetched('[primaryContact, alternativeContact, contacts]');
+
+        return terminal.results || terminal;
     }
 }
 
