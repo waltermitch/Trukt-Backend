f3d39e455c06e42fbfd20715247d26cf24ac2096
waltermitch <devmitch256@gmail.com>
2021-07-01 15:58:35 -0700

    STS-1448 added even more test cases for each test method

--- jest/PicklistController.test.js
+++ jest/PicklistController.test.js
@@ -25,26 +25,74 @@ describe('Test picklist controller functions', () =>
 
     it('snake case string should be cleaned up', () =>
     {
-        expect(PicklistController.cleanUpSnakeCase('foo_bar')).toBe('fooBar');
+        const testVals = [
+            { label: 'foo_bar', expectedLabel: 'fooBar' },
+            { label: 'King_Mufasa', expectedLabel: 'KingMufasa' },
+            { label: 'general_TaosChicken', expectedLabel: 'generalTaosChicken' },
+            { label: 'may%th_2018', expectedLabel: 'may%th2018' }
+        ];
+        for (const testVal of testVals)
+
+            expect(PicklistController.cleanUpSnakeCase(testVal.label)).toBe(testVal.expectedLabel);
+
     });
 
     it('camel case string should be cleaned up', () =>
     {
-        expect(PicklistController.cleanUpCamelCase('fooBar')).toBe('foo Bar');
+        const testVals = [
+            { label: 'fooBar', expectedLabel: 'foo Bar' },
+            { label: 'KingMufasa', expectedLabel: 'King Mufasa' },
+            { label: 'general_TaosChicken', expectedLabel: 'general_Taos Chicken' },
+            { label: 'maY%TH_2018', expectedLabel: 'ma Y%TH_2018' }
+        ];
+        for (const testVal of testVals)
+
+            expect(PicklistController.cleanUpCamelCase(testVal.label)).toBe(testVal.expectedLabel);
+
     });
 
     it('string should be in camel case', () =>
     {
-        expect(PicklistController.setCamelCase('foo bar')).toBe('fooBar');
+        const testVals = [
+            { label: 'foo Bar', expectedLabel: 'fooBar' },
+            { label: 'dub step is weird', expectedLabel: 'dubStepIsWeird' },
+            { label: 'sunlight hurts my eyes', expectedLabel: 'sunlightHurtsMyEyes' },
+            { label: '123dad Mad', expectedLabel: '123dadMad' },
+            { label: '123dad 132 MadLad', expectedLabel: '123dad132MadLad' }
+        ];
+        for (const testVal of testVals)
+
+            expect(PicklistController.setCamelCase(testVal.label)).toBe(testVal.expectedLabel);
+
     });
 
     it('strings should have white space cleared out', () =>
     {
-        expect(PicklistController.cleanUpWhitespace('    Foo     Bar    ')).toBe('Foo Bar');
+        const testVals = [
+            { label: 'foo     Bar    ', expectedLabel: 'foo Bar' },
+            { label: 'dub   step    is weird   ', expectedLabel: 'dub step is weird' },
+            { label: 'sunlight hurts my eyes', expectedLabel: 'sunlight hurts my eyes' },
+            { label: '123dad Mad', expectedLabel: '123dad Mad' },
+            { label: '  ( )     123dad 132 MadLad        ', expectedLabel: '( ) 123dad 132 MadLad' }
+        ];
+        for (const testVal of testVals)
+
+            expect(PicklistController.cleanUpWhitespace(testVal.label)).toBe(testVal.expectedLabel);
+
     });
 
     it('string should be capitalized', () =>
     {
-        expect(PicklistController.capWord('foo bar(uwu test me pwease)')).toBe('Foo Bar(Uwu Test Me Pwease)');
+        const testVals = [
+            { label: 'foo bar(uwu test me pwease)', expectedLabel: 'Foo Bar(Uwu Test Me Pwease)' },
+            { label: 'dub   step    is weird   ', expectedLabel: 'Dub   Step    Is Weird   ' },
+            { label: 'sunlight hurts my eyes', expectedLabel: 'Sunlight Hurts My Eyes' },
+            { label: '123dad Mad', expectedLabel: '123dad Mad' },
+            { label: '       (123da)d 132 MadLad        ', expectedLabel: '       (123da)D 132 MadLad        ' }
+        ];
+        for (const testVal of testVals)
+
+            expect(PicklistController.capWord(testVal.label)).toBe(testVal.expectedLabel);
+
     });
 });
