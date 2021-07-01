40d8c3b9e3ca049b76ab0734d2cf833a82ca357c
waltermitch <devmitch256@gmail.com>
2021-07-01 15:22:59 -0700

    STS-1448 added test case

--- jest/PicklistController.test.js
+++ jest/PicklistController.test.js
@@ -10,6 +10,7 @@ describe('Test picklist controller functions', () =>
             { label: 'pickup truck(5th wheel)', value: 4, expectedLabel: 'Pickup Truck(5th Wheel)', expectedValue: 4 },
             { label: 'trailer5thWheel', value: 'trailer5thWheel', expectedLabel: 'Trailer5th Wheel', expectedValue: 'trailer5thWheel' },
             { label: 'dynasty_warriorsGreat_Movie??', value: '22', expectedLabel: 'Dynasty Warriors Great Movie??', expectedValue: '22' },
+            { label: 'Ross_Great_Quoter', value: 23, expectedLabel: 'Ross Great Quoter', expectedValue: 23 },
         ];
         for (const testVal of testVals)
         {
