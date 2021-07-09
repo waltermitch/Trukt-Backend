ff7804d9f6925af20b84549bd2e5c9d7a4c004d0
waltermitch <devmitch256@gmail.com>
2021-07-09 11:56:41 -0700

    [STS-1452] added express methods

--- Classes/HttpControllers/IndexNumberController.js
+++ Classes/HttpControllers/IndexNumberController.js
@@ -4,6 +4,53 @@ const { orderNumberRegex } = require('../Utils/Regexes');
 
 class IndexNumberController extends HttpRouteController
 {
+
+    static async nextOrderNumber(req, res, next)
+    {
+        try
+        {
+            const result = await IndexNumberService.nextOrderNumber();
+            res.status(200);
+            res.json({ orderNumber: result });
+
+        }
+        catch (err)
+        {
+            next(err);
+        }
+    }
+
+    static async nextJobNumber(req, res, next)
+    {
+        if (req.query?.order)
+        {
+            if (orderNumberRegex.test(req.query.order))
+            {
+                try
+                {
+                    const result = await IndexNumberService.nextJobNumber(req.query.order);
+                    res.status(200);
+                    res.json({ jobNumber: result });
+
+                }
+                catch (err)
+                {
+                    next(err);
+                }
+            }
+            else
+            {
+                res.status(400);
+                res.send('invalid order number');
+            }
+        }
+        else
+        {
+            res.status(400);
+            res.send('missing order number');
+        }
+    }
+
     async handleGet(context, req)
     {
         const res = {};
