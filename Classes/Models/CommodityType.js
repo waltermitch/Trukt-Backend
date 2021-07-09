e8d3d3808d97f6f30a671650a26ef47411f41d39
waltermitch <devmitch256@gmail.com>
2021-07-09 15:42:44 -0700

    STS-1419 created commodity type class

---
+++ Classes/Models/CommodityType.js
@@ -0,0 +1,28 @@
+const BaseModel = require('./BaseModel');
+
+class CommodityType extends BaseModel
+{
+    static get tableName()
+    {
+        return 'rcgTms.commodityTypes';
+    }
+
+    static get idColumn()
+    {
+        return 'id';
+    }
+
+    static get relationMappings()
+    {
+        return {
+            relation: BaseModel.HasManyRelation,
+            modeClass: require('./Commodity'),
+            join: {
+                from: 'rcgTms.commodityTypes.id',
+                to: 'rcgTms.commodities.type'
+            }
+        };
+    }
+}
+
+module.exports = CommodityType;
