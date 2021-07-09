861ff9d7bca757bd9cbe119481feb2764119609a
waltermitch <devmitch256@gmail.com>
2021-07-09 15:45:18 -0700

    STS-1419 added vehicle and vehicle type to commodity class

--- Classes/Models/Commodity.js
+++ Classes/Models/Commodity.js
@@ -11,6 +11,7 @@ class Commodity extends BaseModel
     {
         return 'guid';
     }
+
     static get relationMappings()
     {
         return {
@@ -26,10 +27,36 @@ class Commodity extends BaseModel
                     },
                     to: 'rcgTms.orderStops.guid'
                 }
+            },
+            vehicle: {
+                relation: BaseModel.BelongsToOneRelation,
+                modelClass: require('./Vehicle'),
+                join: {
+                    from: 'rcgTms.commodities.vehicleId',
+                    to: 'rcgTms.vehicles.id'
+                }
+            },
+            vehicleType: {
+                relation: BaseModel.BelongsToOneRelation,
+                modelClass: require('./CommodityType'),
+                join: {
+                    from: 'rcgTms.commodities.type',
+                    to: 'rcgTms.commodityTypes.id'
+                }
             }
         };
     }
 
+    static get modifiers()
+    {
+        return {
+            filterDistinct(builder)
+            {
+                // use distinctOn because we are using pg
+                builder.distinctOn('guid');
+            }
+        };
+    }
 }
 
 module.exports = Commodity;
