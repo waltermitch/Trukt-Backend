10dd92386a61e2b839d0190b69b1476658eac6c3
waltermitch <devmitch256@gmail.com>
2021-07-09 12:21:35 -0700

    [STS-1452] added express server file

---
+++ main.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const BaseModel = require('./Classes/Models/BaseModel');
+const fs = require('fs');
+const HttpErrorHandler = require('./Classes/HttpErrorHandler');
+require('./local.settings');
+
+const store = new KnexSessionStore
+    ({
+        knex: BaseModel.knex(),
+        tableName: 'sessions'
+    });
+
+const app = express();
+
+const sessionConfig = {
+    secret: process.env['node.secret'],
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        // 1 day
+        maxAge: 1 * 24 * 60 * 60 * 1000
+    },
+    store
+};
+
+switch (process.env.ENV)
+{
+    case 'dev':
+    case 'development':
+        sessionConfig.cookie.secure = true;
+        break;
+    case 'staging':
+        sessionConfig.cookie.secure = true;
+        break;
+    case 'prod':
+    case 'production':
+        sessionConfig.cookie.secure = true;
+        break;
+    default:
+        sessionConfig.cookie.secure = false;
+}
+
+app.use(session(sessionConfig));
+app.use(express.json());
+
+// wanted to have a dynamic way to add routes without having to modify main.js
+const filepaths = fs.readdirSync('./Routes');
+for (const filepath of filepaths)
+{
+    const router = require(`./Routes/${filepath}`);
+    app.use(router);
+}
+
+app.all('*', (req, res) => { res.status(404); res.json(); });
+app.use(HttpErrorHandler);
+
+app.listen(process.env.PORT, (err) =>
+{
+    if (err) console.log('there is an error lol');
+    console.log('listening on port ', process.env.PORT);
+});
