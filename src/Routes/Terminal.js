c417905fccb64d9dd04615b8bb9b59680495c32a
waltermitch <devmitch256@gmail.com>
2021-07-09 17:25:28 -0700

    [STS-1452] setup with iisnode for azure

--- src/Routes/Terminal.js
+++ src/Routes/Terminal.js
@@ -2,7 +2,7 @@ const controller = require('../HttpControllers/TerminalController');
 const router = require('express').Router();
 const { uuidRegexStr } = require('../Utils/Regexes');
 
-const prefix = '/terminal';
+const prefix = '/node/express/terminal';
 
 router
     .get(`${prefix}/:terminalGuid(${uuidRegexStr})`, controller.constructor.getByGuid)
