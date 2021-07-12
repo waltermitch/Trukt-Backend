bc284ae6b6751fb31026726ef1f0ad15ecb44252
waltermitch <devmitch256@gmail.com>
2021-07-12 11:22:33 -0700

    [STS-1419] merge with dev

--- src/HttpControllers/AccountController.js
+++ src/HttpControllers/AccountController.js
@@ -4,6 +4,39 @@ const { uuidRegex } = require('../Utils/Regexes');
 
 class AccountController extends HttpRouteController
 {
+    static async getAccount(req, res)
+    {
+        const result = await AccountService.getById(req.params.accountType, req.params.accountId);
+        if (result.length > 0)
+        {
+            res.status(200);
+            res.json(result[0]);
+        }
+        else
+        {
+            res.status(404);
+            res.send();
+        }
+    }
+
+    static async searchAccount(req, res)
+    {
+        if (req.query?.search)
+        {
+            const result = await AccountService.searchByType(req.params.accountType, req.query);
+            res.status(200);
+            res.send(result);
+        }
+        else
+        {
+            res.status(400);
+            res.json({ message: 'missing search query field' });
+        }
+    }
+
+    /**
+     * TODO: deprecated
+     */
     async handleGet(context, req)
     {
         const res = {};
---
+++ src/Routes/Terminal.js
@@ -0,0 +1,11 @@
+const controller = require('../HttpControllers/TerminalController');
+const router = require('express').Router();
+const { uuidRegexStr } = require('../Utils/Regexes');
+
+const prefix = '/terminal';
+
+router
+    .get(`${prefix}/:terminalGuid(${uuidRegexStr})`, controller.constructor.getByGuid)
+    .get(`${prefix}`, controller.constructor.search);
+
+module.exports = router;
