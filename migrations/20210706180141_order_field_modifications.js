44d03cc26ca1a3016e3451413f796a9ce3c9175c
waltermitch <devmitch256@gmail.com>
2021-07-21 19:23:14 -0700

    [STS-1423] added more columns to the order and job tables

---
+++ migrations/20210706180141_order_field_modifications.js
@@ -0,0 +1,48 @@
+
+exports.up = function (knex)
+{
+    return knex.schema.withSchema('rcg_tms')
+        .alterTable('orders', (table) =>
+        {
+            table.decimal('estimated_distance', 8, 1).unsigned().comment('This is the distance that will be entered by the user when the order is created or updated');
+            table.string('bol', 64).comment('customer bill of lading number');
+            table.string('bol_url', 1024).comment('the url to the customer\'s bill of lading electronical version');
+        })
+        .alterTable('order_jobs', (table) =>
+        {
+            table.enu('inspection_type', null, {
+                useNative: true,
+                enumName: 'inspection_types',
+                existingType: true,
+                schemaName: 'rcg_tms'
+            });
+            table.text('loadboard_instructions').comment('instructions to send to the loadboards');
+            table.decimal('estimated_distance', 8, 1).unsigned().comment('This is the distance that will be entered by the user when the job is created or updated');
+            table.string('bol', 64).comment('customer bill of lading number');
+            table.string('bol_url', 1024).comment('the url to the customer\'s bill of lading electronical version');
+            table.integer('equipment_type_id');
+            table.foreign('equipment_type_id').references('id').inTable('rcg_tms.equipment_types')
+                .onDelete('RESTRICT');
+        });
+};
+
+exports.down = function (knex)
+{
+    return knex.schema.withSchema('rcg_tms')
+        .alterTable('orders', (table) =>
+        {
+            table.dropColumn('estimated_distance');
+            table.dropColumn('bol');
+            table.dropColumn('bol_url');
+        })
+        .alterTable('order_jobs', (table) =>
+        {
+            table.dropForeign('equipment_type_id');
+            table.dropColumn('equipment_type_id');
+            table.dropColumn('loadboard_instructions');
+            table.dropColumn('inspection_type');
+            table.dropColumn('estimated_distance');
+            table.dropColumn('bol');
+            table.dropColumn('bol_url');
+        });
+};
