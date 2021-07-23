94c09c75c870ef304174d22d787547d67bc83f71
waltermitch <devmitch256@gmail.com>
2021-07-23 11:43:29 -0700

    [STS-1431] [STS-1419] added fields to loadboard post, added fields to loadboard table, created  loadboards contact table and inserted records

--- migrations/20210616203648_loadboard_posts.js
+++ migrations/20210616203648_loadboard_posts.js
@@ -1,20 +1,101 @@
 const migration_tools = require('../tools/migration');
 
 const loadboardNames = [
-    { name: 'SUPERDISPATCH' },
-    { name: 'CENTRALDISPATCH' },
-    { name: 'SHIPCARS' },
-    { name: 'DAT' },
-    { name: 'TRUCKSTOP' },
-    { name: 'CARDELIVERYNETWORK' },
-    { name: 'AUTOIMS' }
+    { name: 'SUPERDISPATCH', requires_options: false, allows_bulk: true },
+    { name: 'CENTRALDISPATCH', requires_options: false, allows_bulk: true },
+    { name: 'SHIPCARS', requires_options: false, allows_bulk: true },
+    { name: 'CARDELIVERYNETWORK', requires_options: false, allows_bulk: false },
+    {
+        name: 'DAT',
+        requires_options: true,
+        allows_bulk: true,
+        required_fields: {
+            requiredFields: [
+                'contact',
+                'loadType',
+                'equipmentType',
+                'length',
+                'weight',
+                'commodity',
+                'comment1',
+                'comment2'
+            ]
+        }
+    },
+    {
+        name: 'TRUCKSTOP',
+        requires_options: true,
+        allows_bulk: false,
+        required_fields: {
+            requiredFields: [
+                'contact',
+                'loadType',
+                'equipmentType',
+                'length',
+                'weight'
+            ]
+        }
+    }
 ];
+const loadboardContacts = [
+    {
+        name: 'powersports',
+        phone: '(972)866-4640',
+        email: 'powersports@rcglogistics.com',
+        loadboard: 'DAT'
+    },
+    {
+        name: 'vkuzmenko',
+        phone: '(770)338-3583',
+        email: 'vkuzmenko@rcglogistics.com',
+        loadboard: 'DAT'
+    },
+    {
+        name: 'ship',
+        phone: '(972)866-4640',
+        email: 'ship@rcglogistics.com',
+        loadboard: 'DAT'
+    },
+    {
+        name: 'Gabriel Miranda',
+        phone: '(972)866-4640',
+        email: 'powersports@rcglogistics.com',
+        loadboard: 'TRUCKSTOP'
+    },
+    {
+        name: 'Donald Neverov',
+        phone: '(972)866-4640',
+        email: 'ship@rcglogistics.com',
+        loadboard: 'TRUCKSTOP'
+    },
+    {
+        name: 'YMC',
+        phone: '(770)338-3583',
+        email: 'ymc@rcglogistics.com',
+        loadboard: 'TRUCKSTOP'
+    }
+];
+const proms = [];
+const table_name_lb = 'loadboards';
+const table_name_lb_contact = 'loadboard_contacts';
+const table_name_post = 'loadboard_posts';
 exports.up = function (knex)
 {
-    return knex.schema.withSchema('rcg_tms').createTable('loadboards', (table) =>
+    return knex.schema.withSchema('rcg_tms').createTable(table_name_lb, (table) =>
     {
         table.string('name', 24).primary().notNullable();
-    }).createTable('loadboard_posts', (table) =>
+        table.boolean('requires_options');
+        table.boolean('allows_bulk');
+        table.json('required_fields');
+    }).createTable(table_name_lb_contact, (table) =>
+    {
+        table.increments().primary();
+        table.string('name').notNullable();
+        table.string('phone');
+        table.string('email').notNullable();
+        table.string('loadboard');
+        table.foreign('loadboard').references('name').inTable(`rcg_tms.${table_name_lb}`);
+    }).createTable(table_name_post, (table) =>
     {
         table.increments().primary();
         table.uuid('job_guid').notNullable();
@@ -34,17 +115,26 @@ exports.up = function (knex)
         table.boolean('is_created').defaultTo(true);
         table.boolean('is_posted').defaultTo(false);
         table.boolean('is_synced').defaultTo(false);
+        table.boolean('has_error').defaultTo(false);
         table.json('values');
-        migration_tools.timestamps(knex, table);
+
+        migration_tools.timestamps(table);
+        migration_tools.authors(table);
+
         table.foreign('job_guid').references('guid').inTable('rcg_tms.order_jobs');
-        table.foreign('loadboard').references('name').inTable('rcg_tms.loadboards');
-    }).then(() =>
-    {
-        return knex('loadboards').insert(loadboardNames);
-    });
+        table.foreign('loadboard').references('name').inTable(`rcg_tms.${table_name_lb}`);
+    })
+        .raw(migration_tools.timestamps_trigger(table_name_post))
+        .raw(migration_tools.authors_trigger(table_name_post))
+        .then(() =>
+        {
+            proms.push(knex(table_name_lb).insert(loadboardNames), knex(table_name_lb_contact).insert(loadboardContacts));
+            Promise.all(proms);
+        });
+
 };
 
 exports.down = function (knex)
 {
-    return knex.schema.withSchema('rcg_tms').dropTableIfExists('loadboard_posts').dropTableIfExists('loadboards').raw('DROP TYPE IF EXISTS rcg_tms.post_status');
+    return knex.schema.withSchema('rcg_tms').dropTableIfExists(table_name_post).dropTableIfExists(table_name_lb_contact).dropTableIfExists(table_name_lb).raw('DROP TYPE IF EXISTS rcg_tms.post_status');
 };
