f3ab1823f77ecff7d498a90ecf2e179e5baf5982
waltermitch <devmitch256@gmail.com>
2021-07-26 18:00:41 -0700

    [QBO-32] added jobs to CronManager

--- src/CronJobs/Manager.js
+++ src/CronJobs/Manager.js
@@ -1,4 +1,6 @@
 const Triumph = require('../Triumph/API');
+const QBO = require('../QuickBooks/API');
+const Super = require('../Super/API');
 const Cron = require('node-cron');
 
 const expressions =
@@ -12,19 +14,23 @@ const expressions =
 // every 30 minutes
 Cron.schedule(expressions.thirtyMinutes, async () =>
 {
-
+    await QBO.refreshToken();
 });
 
 // every hour
-Cron.schedule(expressions.hourly, async () =>
-{
-    //
-});
+// Cron.schedule(expressions.hourly, async () =>
+// {
+//     // do stuff here
+// });
 
 // every 2 hours
 
 // daily
 Cron.schedule(expressions.daily, async () =>
 {
+    // kick off job but don't wait for it
+    Super.retryCarrierUpdates();
+
+    // get triumph token
     await Triumph.refreshToken();
 });
