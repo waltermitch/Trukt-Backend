d365b1e93ec78ca7071cf1919f2222c7fe8cffad
waltermitch <devmitch256@gmail.com>
2021-07-29 01:35:33 -0700

    [STS-1300] added EDI 214 routes

--- src/Routes/EDI.js
+++ src/Routes/EDI.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 
 router
     .get('/edi/990/outbound/reject', controller.constructor.reject)
-    .get('/edi/990/outbound/accept', controller.constructor.accept);
+    .get('/edi/990/outbound/accept', controller.constructor.accept)
+    .get('/edi/214/outbound', controller.constructor.outbound214)
+    .put('/edi/214/inbound', controller.constructor.inbound214);
 
 module.exports = router;
