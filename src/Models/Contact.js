8f54942d24cd7aee337c0a779e66007bf29000fa
waltermitch <devmitch256@gmail.com>
2021-08-03 12:02:37 -0700

    [STS-1423] added mixins to the terminal contacts model

--- src/Models/Contact.js
+++ src/Models/Contact.js
@@ -1,4 +1,6 @@
 const BaseModel = require('./BaseModel');
+let FindOrCreateMixin = require('./Mixins/FindOrCreate');
+const { RecordAuthorMixin } = require('./Mixins/RecordAuthors');
 
 class Contact extends BaseModel
 {
@@ -11,6 +13,44 @@ class Contact extends BaseModel
     {
         return 'guid';
     }
+
+    static get uniqueColumns()
+    {
+        return [
+            'terminalGuid',
+            'firstName',
+            'lastName',
+            'phoneNumber'
+        ];
+    }
+
+    getColumnOp(colname, colvalue)
+    {
+        if (colname === 'terminalGuid')
+        {
+            return '=';
+        }
+
+        return typeof colvalue === 'string' ? 'ilike' : '=';
+    }
+
+    uniqueKey()
+    {
+        return [
+            this.firstName,
+            this.lastName,
+            this.phoneNumber,
+            this.terminalGuid || this.terminal?.guid
+        ].join(';');
+    }
+
 }
 
+// deleting the property from require, will affect it globally
+// object assign overwrites the same properties
+FindOrCreateMixin = Object.assign({}, FindOrCreateMixin);
+delete FindOrCreateMixin.getColumnOp;
+
+Object.assign(Contact.prototype, FindOrCreateMixin);
+Object.assign(Contact.prototype, RecordAuthorMixin);
 module.exports = Contact;
