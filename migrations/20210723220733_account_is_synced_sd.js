1ad95d00d67870fc4adc36626bfc42922cfd4eaf
waltermitch <devmitch256@gmail.com>
2021-08-06 13:51:12 -0700

    [QBO-32] mig files added account_is_synced

--- migrations/20210723220733_account_is_synced_sd.js
+++ migrations/20210723220733_account_is_synced_sd.js
@@ -4,9 +4,7 @@ exports.up = function (knex)
 {
     return knex.raw(`
     ALTER TABLE ${table_name}
-    ADD COLUMN is_synced_in_super BOOLEAN
-    
-    ALTER VIEW salesforce.accounts as select is_synced_in_super as is_synced_in_super from ${table_name},`);
+    ADD COLUMN is_synced_in_super BOOLEAN`);
 };
 
 exports.down = function (knex)
