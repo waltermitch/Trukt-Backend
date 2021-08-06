6717d8fed09c0865796d4da8d8f7f3996088d5f0
waltermitch <devmitch256@gmail.com>
2021-08-06 10:20:30 -0700

    [QBO-32] fixed uuidRegex

--- src/Routes/Invoice.js
+++ src/Routes/Invoice.js
@@ -1,11 +1,11 @@
 const controller = require('../HttpControllers/InvoiceController');
-const { uuidRegex } = require('../Utils/Regexes');
+const { uuidRegexStr } = require('../Utils/Regexes');
 const router = require('express').Router();
 
 const prefix = '/invoice';
 
 router
-    .get(`${prefix}/:invoiceId(${uuidRegex})`, controller.constructor.getInvoice)
+    .get(`${prefix}/:invoiceId(${uuidRegexStr})`, controller.constructor.getInvoice)
     .post(`${prefix}`, controller.constructor.createInvoices);
 
 module.exports = router;
