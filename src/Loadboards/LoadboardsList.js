bda52439cbb4751d80ec93eab759d3f4c9741a1a
waltermitch <devmitch256@gmail.com>
2021-08-09 13:49:34 -0700

    [STS-1419] created file to export all loadboards into one file

---
+++ src/Loadboards/LoadboardsList.js
@@ -0,0 +1,8 @@
+const SUPERDISPATCH = require('./Super');
+const SHIPCARS = require('./ShipCars');
+const CENTRALDISPATCH = require('./CentralDispatch');
+const TRUCKSTOP = require('./Truckstop');
+const CARDELIVERYNETWORK = require('./CarDeliveryNetwork');
+const DAT = require('./DAT');
+
+module.exports = { SUPERDISPATCH, SHIPCARS, CENTRALDISPATCH, CARDELIVERYNETWORK, TRUCKSTOP, DAT };
