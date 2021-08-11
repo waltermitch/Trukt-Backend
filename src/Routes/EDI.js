25dfc3f02894fa612ebd72dd52e934023b94cd6b
waltermitch <devmitch256@gmail.com>
2021-08-11 16:58:29 -0700

    [STS-1423] added EDI 204 inbound route

--- src/Routes/EDI.js
+++ src/Routes/EDI.js
@@ -1,9 +1,11 @@
 const controller = require('../HttpControllers/EDIController');
+const orderController = require('../HttpControllers/OrderController');
 const router = require('express').Router();
 
 router
     .get('/edi/990/outbound/reject', controller.constructor.reject)
     .get('/edi/990/outbound/accept', controller.constructor.accept)
+    .post('/edi/204/inbound', controller.constructor.createTender, orderController.constructor.createOrder)
     .get('/edi/214/outbound', controller.constructor.outbound214)
     .put('/edi/214/inbound', controller.constructor.inbound214);
 
