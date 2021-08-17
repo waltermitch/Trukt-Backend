17392d8fca5b1c20d29e02983364d8e8a5fe2802
waltermitch <devmitch256@gmail.com>
2021-08-16 17:47:09 -0700

    [STS-1419] added api error column to laodboard post to store errors from loadboards

--- migrations/20210616203648_loadboard_posts.js
+++ migrations/20210616203648_loadboard_posts.js
@@ -80,6 +80,7 @@ exports.up = function (knex)
         table.boolean('is_posted').defaultTo(false);
         table.boolean('is_synced').defaultTo(false);
         table.boolean('has_error').defaultTo(false);
+        table.string('api_error').comment('error that is returned from the loadboard api');
         table.json('values');
 
         migration_tools.timestamps(table);
