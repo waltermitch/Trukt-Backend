411620efc68373b1c2abd25503bdc1b0caa23771
waltermitch <devmitch256@gmail.com>
2021-08-21 16:14:30 -0700

    [STS-1419] added function to create loads in loadboards

--- src/Services/LoadboardService.js
+++ src/Services/LoadboardService.js
@@ -28,6 +28,24 @@ let dbLoadboardNames;
 class LoadboardService
 {
 
+    static async createPostings(jobId, posts, currentUser)
+    {
+        currentUserGuid = currentUser;
+        const job = await LoadboardService.getAllPostingData(jobId, posts);
+        const payloads = [];
+        let lbPayload;
+        for (const post of posts)
+        {
+            lbPayload = new loadboardClasses[`${post.loadboard}`](job);
+            payloads.push(lbPayload['create']());
+        }
+
+        // sending all payloads as one big object so one big response can be returned
+        // and handler can then use one big transaction to update all records rather
+        // than have a single new transaction for each posting
+        await sender.sendMessages({ body: payloads });
+    }
+
     static async postPostings(jobId, posts, currentUser)
     {
         currentUserGuid = currentUser;
