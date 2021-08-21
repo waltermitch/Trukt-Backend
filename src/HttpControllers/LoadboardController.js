7b3579bd5079eb425ab1d5f8de2361f8c1278362
waltermitch <devmitch256@gmail.com>
2021-08-21 16:21:09 -0700

    [STS-1419] added controller create handler

--- src/HttpControllers/LoadboardController.js
+++ src/HttpControllers/LoadboardController.js
@@ -3,6 +3,24 @@ const LoadboardService = require('../Services/LoadboardService');
 
 class LoadboardController extends HttpRouteController
 {
+    static async handleCreate(req, res)
+    {
+        const posts = req.body.posts;
+
+        try
+        {
+            LoadboardService.checkLoadboardsInput(posts);
+            await LoadboardService.createPostings(req.params.jobId, posts, req.session.userGuid);
+
+            res.status(204).send();
+        }
+        catch (e)
+        {
+            res.status(400);
+            res.json({ message: e });
+        }
+    }
+
     static async handlePost(req, res)
     {
         const posts = req.body.posts;
