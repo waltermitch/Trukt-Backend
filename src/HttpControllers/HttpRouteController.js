c4e8fdd745b0fa372dc21e4d77dcdd5bb1e65657
waltermitch <devmitch256@gmail.com>
2021-08-20 21:13:49 -0700

    [QBO-32] made http handling global method

--- src/HttpControllers/HttpRouteController.js
+++ src/HttpControllers/HttpRouteController.js
@@ -2,139 +2,28 @@ const ErrorHandler = require('../ErrorHandler');
 
 class HttpRouteController
 {
-    /* eslint-disable */
-    async handleGet()
+    static async http(req, res, next)
     {
-        return { 'status': 501 };
-    }
-
-    async handlePost()
-    {
-        return { 'status': 501 };
-    }
-
-    async handlePut()
-    {
-        return { 'status': 501 };
-    }
-
-    async handlePatch()
-    {
-        return { 'status': 501 };
-    }
-
-    async handleDelete()
-    {
-        return { 'status': 501 };
-    }
-
-    /* eslint-enable */
-    async handleHttp(context, request)
-    {
-        // we get no JS access in the function.json so have to do introspection
-        let response = undefined;
-
-        // response should mimic the http response at the top level
-        // setting the status, body and headers etc.
         try
         {
-            switch (request.method)
-            {
-                case 'GET':
-                    response = await this.handleGet(context, request);
-                    break;
-                case 'POST':
-                    response = await this.handlePost(context, request);
-                    break;
-                case 'PUT':
-                    response = await this.handlePut(context, request);
-                    break;
-                case 'PATCH':
-                    response = await this.handlePatch(context, request);
-                    break;
-                case 'DELETE':
-                    response = await this.handleDelete(context, request);
-                    break;
-            }
+            // add auth step in here eventually
+            // req.session = do auth stuff
+
+            await next(req, res);
         }
         catch (err)
         {
-            context.log(err);
+            // handle error in here
+            console.log(err);
+            const error = new ErrorHandler(err);
 
-            // handle generic errors here?
-            const errResp = new ErrorHandler(context, err);
-            response =
-            {
-                status: errResp.status,
-                body: errResp.data
-            };
-        }
-        finally
-        {
-            if (response)
-
-                // conditionally assign the properties
-                // dont assign if they are falsey
-                for (const prop in response)
-                    if (response[prop])
-                        context.res[prop] = response[prop];
-
-            // set the default Content-Type to application/json
-            if (!context?.res?.headers?.['Content-Type'])
-                context.res.headers['Content-Type'] = 'application/json';
+            res.status(error.status || 500);
+            res.json({ 'error': error.data || 'Internal Server Error' });
         }
 
-    }
-
-    static async next(context, func, params)
-    {
-        if (!Array.isArray(params)) params = [params];
-
-        let response = {};
-
-        try
-        {
-            response = await func(context, ...params);
-        }
-        catch (err)
-        {
-            response = new ErrorHandler(context, err);
-        }
-
-        context.res =
-        {
-            headers: { 'Content-Type': 'application/json' },
-            status: response?.status,
-            body: response
-        };
-    }
-
-    static async timer(context, func)
-    {
-        let response = {};
-
-        try
-        {
-            response = await func();
-        }
-        catch (err)
-        {
-            response = new ErrorHandler(context, err);
-        }
-        finally
-        {
-            context.log(response);
-        }
-    }
-
-    static resolve(context, response)
-    {
-        context.res = {
-            headers: { 'Content-Type': 'application/json' },
-            status: response?.status,
-            body: response
-        };
     }
 }
+
+global.http = HttpRouteController.http;
 
 module.exports = HttpRouteController;
