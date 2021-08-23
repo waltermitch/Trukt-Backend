91115ec8f52378bcb5d404cdc1019b9fd4597963
waltermitch <devmitch256@gmail.com>
2021-08-23 00:40:42 -0700

    [QBO-32] saving work on Expense routes

---
+++ src/Routes/Expense.js
@@ -0,0 +1,12 @@
+const controller = require('../HttpControllers/ExpenseController.js');
+const { uuidRegexStr } = require('../Utils/Regexes');
+const router = require('express').Router();
+
+const prefix = '/expense';
+router
+    .get(`${prefix}/:expenseId(${uuidRegexStr})`, (req, res) => http(req, res, controller.get))
+    .post(`${prefix}`, (req, res) => http(req, res, controller.post))
+    .patch(`${prefix}/:expenseId(${uuidRegexStr})`, (req, res) => http(req, res, controller.patch))
+    .patch(`${prefix}`, (req, res) => http(req, res, controller.patch));
+
+module.exports = router;
