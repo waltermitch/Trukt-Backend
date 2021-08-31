8cd897637b12ccea6c856b509ac2bbdd826d7522
waltermitch <devmitch256@gmail.com>
2021-08-31 17:00:32 -0500

    [STS-1480] feat: get order filters and picklist
    [feat-1480] change date filter name
    
    [feat-1480] fix comments
    
    [feat-1480] change get order route to post order/getorders

--- migrations/20210820155300_status_log_types.js
+++ migrations/20210820155300_status_log_types.js
@@ -1,7 +1,17 @@
 const TABLE_NAME = 'status_log_types';
 const SCHEMA_NAME = 'rcg_tms';
 
-const status_log_type_records = [{ id: 1, category: 'order', name: 'Created' }, { id: 2, category: 'order', name: 'Posted to' }, { id: 3, category: 'order', name: 'Un-Posted from' }];
+const status_log_type_records = [
+    {
+        id: 1, category: 'order', name: 'Created', order_filter_label: 'Created On'
+    },
+    {
+        id: 2, category: 'order', name: 'Posted to', order_filter_label: null
+    },
+    {
+        id: 3, category: 'order', name: 'Un-Posted from', order_filter_label: null
+    }
+];
 
 exports.up = function (knex)
 {
@@ -10,6 +20,7 @@ exports.up = function (knex)
         table.integer('id').primary();
         table.string('category', 16).notNullable();
         table.string('name', 32).notNullable();
+        table.string('order_filter_label', 32).default(null);
         table.unique(['category', 'name']);
     }).then(() =>
     {
