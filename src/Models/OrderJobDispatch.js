c762d9062802443a5e303226f4dfb6cde4caff2c
waltermitch <devmitch256@gmail.com>
2021-09-07 12:52:06 -0700

    [STS-1413] created model for job dispatches

---
+++ src/Models/OrderJobDispatch.js
@@ -0,0 +1,57 @@
+const BaseModel = require('./BaseModel');
+const { RecordAuthorMixin } = require('./Mixins/RecordAuthors');
+
+class OrderJobDispatch extends BaseModel
+{
+    static get tableName()
+    {
+        return 'rcgTms.orderJobDispatches';
+    }
+
+    static get idColumn()
+    {
+        return 'guid';
+    }
+
+    static get relationMappings()
+    {
+        const SFContact = require('./SFContact');
+        return {
+            job: {
+                relation: BaseModel.BelongsToOneRelation,
+                modelClass: require('./OrderJob'),
+                join: {
+                    from: 'rcgTms.orderJobDispatches.jobGuid',
+                    to: 'rcgTms.orderJobs.guid'
+                }
+            },
+            vendor: {
+                relation: BaseModel.BelongsToOneRelation,
+                modelClass: require('./SFAccount'),
+                join: {
+                    from: 'rcgTms.orderJobDispatches.vendorGuid',
+                    to: 'salesforce.accounts.guid'
+                }
+            },
+            vendorContact: {
+                relation: BaseModel.BelongsToOneRelation,
+                modelClass: SFContact,
+                join: {
+                    from: 'rcgTms.orderJobDispatches.vendorContactGuid',
+                    to: 'salesforce.contacts.guid'
+                }
+            },
+            vendorAgent: {
+                relation: BaseModel.BelongsToOneRelation,
+                modelClass: SFContact,
+                join: {
+                    from: 'rcgTms.orderJobDispatches.vendorAgentGuid',
+                    to: 'salesforce.contacts.guid'
+                }
+            }
+        };
+    }
+}
+
+Object.assign(OrderJobDispatch.prototype, RecordAuthorMixin);
+module.exports = OrderJobDispatch;
