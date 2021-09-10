167bfaceae3100c16e98d391178d76b27658ced2
waltermitch <devmitch256@gmail.com>
2021-09-09 17:34:54 -0700

    [STS-1413] added dispatching and undispathing functions

--- src/Services/LoadboardService.js
+++ src/Services/LoadboardService.js
@@ -2,18 +2,26 @@ const Loadboard = require('../Models/Loadboard');
 const LoadboardPost = require('../Models/LoadboardPost');
 const LoadboardContact = require('../Models/LoadboardContact');
 const Job = require('../Models/OrderJob');
+const SFAccount = require('../Models/SFAccount');
+const InvoiceLine = require('../Models/InvoiceLine');
+const currency = require('currency.js');
 
 const StatusManagerHandler = require('../EventManager/StatusManagerHandler');
 const loadboardClasses = require('../Loadboards/LoadboardsList');
 
 const { ServiceBusClient } = require('@azure/service-bus');
+const SFContact = require('../Models/SFContact');
+const InvoiceBill = require('../Models/InvoiceBill');
+const OrderStop = require('../Models/OrderStop');
+const OrderJobDispatch = require('../Models/OrderJobDispatch');
 
 const connectionString = process.env['azure.servicebus.loadboards.connectionString'];
-const queueName = 'loadboard_posts_outgoing';
+const queueName = 'test_queue';
 const sbClient = new ServiceBusClient(connectionString);
 const sender = sbClient.createSender(queueName);
 
 let currentUserGuid = '00000000-0000-0000-0000-000000000000';
+const dispatchableLoadboards = ['SUPERDISPATCH', 'SHIPCARS'];
 let dbLoadboardNames;
 
 (async function ()
@@ -74,6 +82,7 @@ class LoadboardService
                 lbPayload = new loadboardClasses[`${post.loadboard}`](job);
                 payloads.push(lbPayload['post']());
             }
+            console.log(payloads);
 
             // sending all payloads as one big object so one big response can be returned
             // and handler can then use one big transaction to update all records rather
@@ -138,6 +147,165 @@ class LoadboardService
         return payloads;
     }
 
+    static async dispatchToLoadboard(jobId, body)
+    {
+
+    }
+
+    static async dispatchInternally(jobId, body)
+    {
+        const carrier = (await SFAccount.query().modify('byId', body.carrier.guid).modify('carrier'))[0];
+        const driver = body.driver;
+
+        if (!carrier)
+        {
+            throw new Error('carrier not found, please pass in a valid guid, salesforce id, or dot number');
+        }
+        else if (carrier.status == 'Inactive' || carrier.status == 'Blacklist')
+        {
+            throw new Error(`Carrier has a status of ${carrier.status} and cannot be dispatched to without manager approval`);
+        }
+
+        let carrierContact = driver.guid == null ? driver : (await SFContact.query().modify('byId', driver.guid))[0];
+
+        // first check if the contact exists based off guid that was passed in
+        if (carrierContact.guid == null)
+        {
+            if (driver.name == null || driver.email == null || driver.phoneNumber == null)
+            {
+                throw new Error('Please pass in valid driver object that includes a proper name, email, and phone');
+            }
+            carrierContact = SFContact.fromJson(carrierContact);
+            carrierContact.accountId = carrier.sfId;
+            carrierContact = await SFContact.query().insertAndFetch(carrierContact);
+        }
+        else
+        {
+            console.log('not creating new contact');
+            if (carrierContact.accountId != carrier.sfId)
+            {
+                throw new Error('Please pass in valid driver for carrier');
+            }
+        }
+
+        if (body.loadboard != null && !dispatchableLoadboards.includes(body.loadboard))
+        {
+            throw new Error(`${body.loadboard} cannot be dispatched to, you can only dispatch to ${dispatchableLoadboards}`);
+        }
+        let job = {};
+
+        // if dispatching internally, no need to worry about making sure the load is in
+        // another loadboard, otherwise first create a posting record and dispatch
+        if (!body.loadboard)
+        {
+            job = await Job.query().findById(jobId).withGraphFetched('[stops(distinct), commodities(distinct, isNotDeleted), bills.lines(isNotDeleted, transportOnly).item, dispatches(activeDispatch)]');
+            const stops = await this.getFirstAndLastStops(job.stops);
+            Object.assign(job, stops);
+
+            // this.combineCommoditiesWithLines(job.commodities, job.bills[0], 'bill');
+        }
+        else
+        {
+            job = await this.getAllPostingData(jobId, [{ loadboard: body.loadboard }]);
+        }
+
+        if (job.isDummy)
+        {
+            throw new Error('Cannot dispatch dummy job');
+        }
+
+        if (job.dispatches.length != 0)
+        {
+            throw new Error('Cannot dispatch with already active load offer');
+        }
+
+        job.vendorGuid = carrier.guid;
+        job.vendorAgentGuid = carrierContact.guid;
+
+        const jobTotalPrice = job.bills[0].lines.reduce((a, expenseLine) =>
+        {
+            return a.add(currency(expenseLine.amount));
+        }, currency(0));
+
+        const bill = job.bills[0];
+        await InvoiceBill.query().patch({ paymentMethodId: body.paymentMethod, paymentTermId: body.paymentTerm, updatedByGuid: currentUserGuid }).findById(bill.guid);
+        if (body.price !== jobTotalPrice.value)
+        {
+            const lines = bill.lines;
+            const distribution = currency(body.price).distribute(job.bills[0].lines.length);
+            for (let i = 0; i < lines.length; i++)
+            {
+                const billLine = lines[i];
+                const amount = distribution[i].value;
+                await InvoiceLine.query().patch({ amount: amount, updatedByGuid: currentUserGuid }).findById(billLine.guid);
+            }
+        }
+
+        job.pickup.dateScheduledType = body.pickup.dateType;
+        job.pickup.dateScheduledStart = body.pickup.startDate;
+        job.pickup.dateScheduledEnd = body.pickup.dateType == 'estimated' ? body.pickup.endDate : null;
+        job.pickup.updatedByGuid = currentUserGuid;
+        await OrderStop.query().patch(job.pickup).findById(job.pickup.guid);
+
+        job.delivery.dateScheduledType = body.delivery.dateType;
+        job.delivery.dateScheduledStart = body.delivery.startDate;
+        job.delivery.dateScheduledEnd = body.delivery.dateType == 'estimated' ? body.delivery.endDate : null;
+        job.delivery.updatedByGuid = currentUserGuid;
+        await OrderStop.query().patch(job.delivery).findById(job.delivery.guid);
+
+        await Job.query().patch({ vendorGuid: carrier.guid, vendorAgentGuid: carrierContact.guid, updatedByGuid: currentUserGuid }).findById(job.guid);
+        job.vendor = carrier;
+        job.vendorAgent = driver;
+
+        // const lbPost = body.loadboard == null ? null : job.postObjects[`${body.loadboard}`].guid;
+        const lbPost = body.loadboard == null ? null : job.postObjects[`${body.loadboard}`].guid;
+        const dispatch = OrderJobDispatch.fromJson({
+            jobGuid: job.guid,
+            loadboardPostGuid: lbPost,
+            vendorGuid: carrier.guid,
+            vendorAgentGuid: driver.guid,
+            externalGuid: null,
+            isActive: true,
+            isCanceled: false,
+            paymentTerm: body.paymentTerm,
+            paymentMethod: body.paymentMethod,
+            price: body.price,
+            createdByGuid: currentUserGuid
+        });
+
+        job.dispatch = await OrderJobDispatch.query().insertAndFetch(dispatch);
+
+        let message = {};
+        if (lbPost)
+        {
+            const lbPayload = new loadboardClasses[`${body.loadboard}`](job);
+            message = [lbPayload['dispatch']()];
+
+            // await sender.sendMessages({ body: message });
+        }
+
+        return dispatch;
+    }
+
+    static async cancelDispatch(jobGuid)
+    {
+        const dispatch = (await OrderJobDispatch.query().withGraphJoined('loadboardPost').where({ 'rcgTms.orderJobDispatches.jobGuid': jobGuid, isActive: true, isCanceled: false }))[0];
+
+        await OrderJobDispatch.query().patch({ isActive: false, isCanceled: true, updatedByGuid: currentUserGuid }).findById(dispatch.guid);
+
+        // const fakeJob = { postObjects: {} };
+
+        // fakeJob.postObjects[`${dispatch.loadboardPost.loadboard}`] = dispatch.loadboardPost;
+        // fakeJob.dispatch = dispatch;
+
+        // const lbPayload = new loadboardClasses[`${dispatch.loadboardPost.loadboard}`](fakeJob);
+        // const message = [lbPayload['undispatch']()];
+        // console.log(message);
+        // await sender.sendMessages({ body: message });
+
+        return dispatch;
+    }
+
     // This method gets all the job information and creates new post records based on the posts
     // list that is sent in if needed
     static async getAllPostingData(jobId, posts)
@@ -149,7 +317,7 @@ class LoadboardService
             stops(distinct).[primaryContact, terminal], 
             loadboardPosts(getExistingFromList),
             equipmentType, 
-            bills.lines.item,
+            bills.lines(isNotDeleted, transportOnly).item,
             dispatcher
         ]`).modifiers({
             getExistingFromList: builder => builder.modify('getFromList', loadboardNames)
@@ -157,9 +325,11 @@ class LoadboardService
 
         await this.createPostRecords(job, posts);
         this.combineCommoditiesWithLines(job.commodities, job.order.invoices[0], 'invoice');
-        delete job.order.invoices;
+
+        // delete job.order.invoices;
         this.combineCommoditiesWithLines(job.commodities, job.bills[0], 'bill');
-        delete job.bills;
+
+        // delete job.bills;
         const stops = await this.getFirstAndLastStops(job.stops);
         Object.assign(job, stops);
         delete job.stops;
@@ -295,7 +465,6 @@ class LoadboardService
             return stop1.sequence - stop2.sequence;
         });
 
-        // return [stops[0], stops[stops.length - 1]];
         return { pickup: stops[0], delivery: stops[stops.length - 1] };
     }
 
