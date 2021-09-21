1a1f2f31a7076c76847af7796798882561aadef3
waltermitch <devmitch256@gmail.com>
2021-09-21 12:55:27 -0700

    [STS-1413] changed table vendor fields to string instead of uuid to be consistent with other tables that also reference salesforce accounts

--- migrations/20210824173917_order_job_dispatch.js
+++ migrations/20210824173917_order_job_dispatch.js
@@ -11,18 +11,17 @@ exports.up = function (knex)
         table.uuid('loadboard_post_guid');
 
         // carrier information
-        table.uuid('vendor_guid', 100).notNullable().comment('The account completing the job for the company, usually a carrier account');
-        table.uuid('vendor_contact_guid', 100).comment('The vendor\'s primary contact');
-        table.uuid('vendor_agent_guid', 100).comment('The vendor\'s agent that will be doing the job, usually a driver or another worker');
+        table.string('vendor_guid', 100).notNullable().comment('The account completing the job for the company, usually a carrier account');
+        table.foreign('vendor_guid').references('guid__c').inTable('salesforce.account');
+        table.string('vendor_contact_guid', 100).comment('The vendor\'s primary contact');
+        table.foreign('vendor_contact_guid').references('guid__c').inTable('salesforce.contact');
+        table.string('vendor_agent_guid', 100).comment('The vendor\'s agent that will be doing the job, usually a driver or another worker');
+        table.foreign('vendor_agent_guid').references('guid__c').inTable('salesforce.contact');
 
         table.string('external_guid', 100).comment('The external guid from the dispatch action returned from loadboards');
-        table.string('is_pending').comment('Indicates if the dispatch was sent out but has not been accepted or canceled yet');
-
-        // accepted information
-        // table.boolean('is_accepted').comment('Indicates if the dispatch has been accepted and is now being fulfilled');
-        // table.uuid('accepted_by').comment('Guid of the user that accepted the offer');
-        // table.string('accepted_type').comment('Type of user that accepted the dispatch');
-        // table.timestamp('date_accepted').comment('The datetime the dispatch was accepted');
+        table.boolean('is_pending').comment('Indicates if the dispatch was sent out and is waiting to be accepted or declined by the carrier or dispatcher');
+        table.boolean('is_accepted');
+        table.boolean('is_canceled');
 
         // financial information
         table.integer('payment_term_id').comment('The payment term id selected at the time of dispatching');
@@ -30,12 +29,6 @@ exports.up = function (knex)
         table.decimal('price', 15, 2).unsigned().comment('The price the job was dispatched for');
         table.foreign('payment_term_id').references('id').inTable('rcg_tms.invoice_bill_payment_terms');
         table.foreign('payment_method_id').references('id').inTable('rcg_tms.invoice_bill_payment_methods');
-
-        // canceled information
-        // table.boolean('is_canceled').comment('Indicates if dispatch was canceled');
-        // table.uuid('canceled_by').comment('Guid of the user that canceled the dispatch');
-        // table.string('canceled_type', 10).comment('Type of user that canceled the dispatch');
-        // table.timestamp('date_canceled').comment('The datetime the dispatch was canceled on');
 
         table.foreign('loadboard_post_guid').references('guid').inTable('rcg_tms.loadboard_posts');
         migration_tools.timestamps(table);
