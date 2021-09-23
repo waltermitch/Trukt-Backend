6f3776ceebacb114ca61f32443fd34085634f335
waltermitch <devmitch256@gmail.com>
2021-09-23 13:31:23 -0700

    [STS-1505] added currentUser to insert/update/delete routes

--- src/Services/AttachmentService.js
+++ src/Services/AttachmentService.js
@@ -11,6 +11,9 @@ class AttachmentService
     {
         const attachment = await Attachment.query().findById(guid);
 
+        if (!attachment)
+            return null;
+
         // get sas
         const sas = AzureStorage.getSAS();
 
@@ -25,7 +28,10 @@ class AttachmentService
 
     static async searchByParent({ parent, parentType, attachmentType, visibility })
     {
-        const builder = Attachment.query().where('parent', '=', `${parent}`).where('parent_table', '=', `${parentType}`);
+        const builder = Attachment.query()
+            .where('parent', '=', `${parent}`)
+            .where('parent_table', '=', `${parentType}`)
+            .where('is_deleted', '=', false);
 
         if (attachmentType)
             builder.where('type', '=', attachmentType);
@@ -48,7 +54,7 @@ class AttachmentService
         return attachments;
     }
 
-    static async insert(files, opts)
+    static async insert(files, opts, currentUser)
     {
         if (['job'].includes(opts.parentType))
             return { 'status': 400, 'data': 'Not An Allowed parentType' };
@@ -75,26 +81,28 @@ class AttachmentService
                 'name': files[i].originalname,
                 'parent': opts.parent,
                 'parent_table': opts.parentType,
-                'visibility': opts.visibility
+                'visibility': opts.visibility,
+                'createdByGuid': currentUser
             };
 
             // compose full path of file
             const fullPath = `${path}/${guid}/${file.name}`;
 
-            await Promise.all([AzureStorage.storeBlob(fullPath, files[i].buffer), Attachment.query().insert(file)]);
+            const res = await Promise.all([AzureStorage.storeBlob(fullPath, files[i].buffer), Attachment.query().insert(file)]);
 
-            urls.push({ 'url': file.url + sas, 'name': file.name, 'guid': guid });
+            urls.push({ 'url': res[1].url + sas, 'name': res[1].name, 'guid': res[1].guid, 'extension': res[1].extension, 'type': res[1].type, 'visibility': res[1].visibility || ['internal'], 'createdByGuid': res[1].createdByGuid, 'createdAt': res[1].dateCreated });
         }
 
         return urls;
     }
 
-    static async update(guid, data)
+    static async update(guid, data, currentUser)
     {
         const payload =
         {
             type: data?.type,
-            visibility: Array.isArray(data?.visibility) ? data?.visibility : data?.visibility?.split(',')
+            visibility: Array.isArray(data?.visibility) ? data?.visibility : data?.visibility?.split(','),
+            updatedByGuid: currentUser
         };
 
         const res = await Attachment.query().patchAndFetchById(guid, payload);
@@ -105,9 +113,15 @@ class AttachmentService
         return res;
     }
 
-    static async delete(guid)
+    static async delete(guid, currentUser)
     {
-        const res = await Attachment.query().deleteById(guid);
+        const payload =
+        {
+            deletedByGuid: currentUser,
+            is_deleted: true
+        };
+
+        const res = await Attachment.query().patchAndFetchById(guid, payload);
 
         return res;
     }
