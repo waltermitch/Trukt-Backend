04cd6fdc6b75ca8af5cb6260287e9d90c8c16a45
waltermitch <devmitch256@gmail.com>
2021-09-23 15:56:40 -0700

    [TBE-2] moved syncUsers into SystemManagementService

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -1,4 +1,3 @@
-const Graph = require('../Azure/Graph');
 const User = require('../Models/User');
 
 class UserService
@@ -17,34 +16,6 @@ class UserService
         return users.results || users;
     }
 
-    static async syncUsersWithAD()
-    {
-        // gets users from AD Employee Group
-        const usersFromAD = await Graph.getGroupMembers(process.env['azure.ad.groupId']);
-
-        // gets users from DB
-        const usersFromDB = await User.query();
-
-        // map db users to map
-        const dbUsers = new Map(usersFromDB.map((user) => [user.guid, user]));
-
-        // for each user upser it to the database
-        for (const user of usersFromAD)
-        {
-            const payload = { name: user.displayName, email: user.mail, guid: user.id };
-
-            await User.query().insert(payload).onConflict('guid').merge();
-        }
-
-        // for all the leftover users - mark as deleted
-        while (dbUsers.size > 0)
-        {
-            const value = dbUsers.values().next().value;
-            dbUsers.delete(value.guid);
-            await User.query().patchAndFetchById(value.guid, { is_deleted: true });
-
-        }
-    }
 }
 
 module.exports = UserService;
