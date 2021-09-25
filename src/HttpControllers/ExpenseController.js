26681afdac9b14a5c7202990e10656a1bf2232ce
waltermitch <devmitch256@gmail.com>
2021-09-24 18:15:12 -0700

    [QBO-33] added search route

--- src/HttpControllers/ExpenseController.js
+++ src/HttpControllers/ExpenseController.js
@@ -6,15 +6,21 @@ class ExpenseController
     {
         const result = await ExpenseService.find(req.params.expenseId);
 
-        if (result)
-        {
-            res.status(200);
-            res.json(result);
-        }
-        else
+        if (!result)
         {
             res.status(404);
             res.json({ 'error': 'No Matches Found' });
+
+        }
+        else if (result.isDeleted)
+        {
+            res.status(404);
+            res.json({ 'error': 'Expense Deleted' });
+        }
+        else
+        {
+            res.status(200);
+            res.json(result);
         }
     }
 
@@ -40,6 +46,26 @@ class ExpenseController
 
         res.status(204).send();
 
+    }
+
+    static async delete(req, res)
+    {
+        const guid = req?.body?.guid || req?.params?.expenseId;
+
+        if (!guid)
+            throw { 'status': 400, 'data': 'Missing Guid' };
+
+        await ExpenseService.delete(guid, req.session.userGuid);
+
+        res.status(204).send();
+    }
+
+    static async search(req, res)
+    {
+        const result = await ExpenseService.search(req.query);
+
+        res.status(200);
+        res.json(result);
     }
 }
 
--- src/Routes/Expense.js
+++ src/Routes/Expense.js
@@ -5,6 +5,7 @@ const router = require('express').Router();
 const prefix = '/expense';
 router
     .get(`${prefix}/:expenseId(${uuidRegexStr})`, (req, res) => http(req, res, controller.get))
+    .get(`${prefix}/search`, (req, res) => http(req, res, controller.search))
     .post(`${prefix}`, (req, res) => http(req, res, controller.post))
     .patch(`${prefix}/:expenseId(${uuidRegexStr})`, (req, res) => http(req, res, controller.patch))
     .patch(`${prefix}`, (req, res) => http(req, res, controller.patch));
