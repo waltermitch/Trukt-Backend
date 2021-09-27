87f89e76f12e7eb118524f79ed73f2bb85066eef
waltermitch <devmitch256@gmail.com>
2021-09-27 16:57:06 -0700

    [QBO-33] added delete expense route

--- src/Routes/Expense.js
+++ src/Routes/Expense.js
@@ -8,6 +8,7 @@ router
     .get(`${prefix}/search`, (req, res) => http(req, res, controller.search))
     .post(`${prefix}`, (req, res) => http(req, res, controller.post))
     .patch(`${prefix}/:expenseId(${uuidRegexStr})`, (req, res) => http(req, res, controller.patch))
-    .patch(`${prefix}`, (req, res) => http(req, res, controller.patch));
+    .patch(`${prefix}`, (req, res) => http(req, res, controller.patch))
+    .delete(`${prefix}/:expenseId(${uuidRegexStr})`, (req, res) => http(req, res, controller.delete));
 
 module.exports = router;
