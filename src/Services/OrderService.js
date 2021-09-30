69432a6234c8cb26f85262c6bdeb1d0306e117ac
waltermitch <devmitch256@gmail.com>
2021-09-30 08:52:14 -0500

    [FIX] Allow to remove reference for contacts, clients and referenceNumber and insert rest of data in schema

--- src/Services/OrderService.js
+++ src/Services/OrderService.js
@@ -913,7 +913,8 @@ class OrderService
             terminals = [],
             stops = [],
             jobs = [],
-            expenses = []
+            expenses = [],
+            ...orderData
         } = orderInput;
 
         try
@@ -982,7 +983,8 @@ class OrderService
                 clientContactGuid: orderContactCreated,
                 stops: stopsToUpdate,
                 jobs: jobsWithExpensesGraph,
-                invoices: orderInvoices
+                invoices: orderInvoices,
+                ...orderData
             });
 
             const orderToUpdate = Order.query(trx).skipUndefined().upsertGraphAndFetch(orderGraph, {
@@ -1025,6 +1027,8 @@ class OrderService
             const { guid: orderContactGuid, ...orderContactData } = orderContact;
             newOrderContactChecked = orderContactData;
         }
+        else if (orderContact && orderChecked === 'removeContact')
+            newOrderContactChecked = { guid: null };
 
         return { newOrderContactChecked, terminalsChecked, stopsChecked };
     }
@@ -1106,7 +1110,9 @@ class OrderService
 
     static async checkContactReference(contact, orderGuid)
     {
-        if (!contact.guid)
+        if (contact.guid === null && Object.keys(contact).length === 1)
+            return 'removeContact';
+        else if (!contact.guid)
             return 'createNewContact';
 
         const searchInOrder = Order.query().count('guid')
@@ -1280,10 +1286,14 @@ class OrderService
     }
     static async createOrderContactCommoditiesTerminalsMap(contactInfo, commoditiesInfo, terminals, currentUser, trx)
     {
-        const { contact, contactRecordType, client } = contactInfo;
+        const { contact = {}, contactRecordType, client } = contactInfo;
         const { commodities, commodityTypes } = commoditiesInfo;
 
-        const orderContactTocreate = OrderService.createSFContact(contact, contactRecordType, client, trx);
+        let orderContactTocreate;
+        if (contact.guid === null && Object.keys(contact).includes('guid'))
+            orderContactTocreate = null;
+        else if (contact && Object.keys(contact).length > 0)
+            orderContactTocreate = OrderService.createSFContact(contact, contactRecordType, client, trx);
 
         const commoditiesToUpdate = commodities?.map(commodity => OrderService.updateCreateCommodity(commodity, commodityTypes, currentUser, trx)) || [];
         const terminalsToUpdate = terminals?.map(terminal => OrderService.updateCreateTerminal(terminal, currentUser, trx)) || [];
