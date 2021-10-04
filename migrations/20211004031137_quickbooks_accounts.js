007437329a537b76c8173a8714295122b8d56fda
waltermitch <devmitch256@gmail.com>
2021-10-03 20:57:52 -0700

    [QBO-32] removed guid requirement for account

--- migrations/20211004031137_quickbooks_accounts.js
+++ migrations/20211004031137_quickbooks_accounts.js
@@ -8,16 +8,15 @@ exports.up = function (knex)
     return knex.schema.withSchema(schemaName)
         .createTable(accountsTable, (table) =>
         {
-            table.uuid('guid').notNullable().unique();
             table.integer('id', { primaryKey: true }).unique().notNullable();
             table.string('name').notNullable();
         })
         .createTable(mappings, (table) =>
         {
-            table.uuid('account_guid').unique().notNullable();
+            table.integer('account_id').unique().notNullable();
             table.integer('item_id').notNullable();
 
-            table.foreign('account_guid').references('guid').inTable(`${schemaName}.${accountsTable}`);
+            table.foreign('account_id').references('id').inTable(`${schemaName}.${accountsTable}`);
             table.foreign('item_id').references('id').inTable('rcg_tms.invoice_bill_line_items');
         });
 };
@@ -26,6 +25,6 @@ exports.down = function (knex)
 {
     // drop schema with tables
     return knex.schema.withSchema(schemaName)
-        .dropTableIfExists(accountsTable)
-        .dropTableIfExists(mappings);
+        .dropTableIfExists(mappings)
+        .dropTableIfExists(accountsTable);
 };
