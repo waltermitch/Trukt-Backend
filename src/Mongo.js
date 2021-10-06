0ec2afb14e894c63e924959771ad304f3eaa5eb9
waltermitch <devmitch256@gmail.com>
2021-10-06 16:51:15 -0700

    [QBO-32] added mutex lock for mongo connections.

--- src/Mongo.js
+++ src/Mongo.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require('mongodb');
+const Lock = require('./MongoLock');
 
 const dbUrl = process.env['mongo.connection.uri'];
 const dbName = process.env['mongo.database'];
@@ -9,60 +10,76 @@ const dbOptions =
     useNewUrlParser: true
 };
 
-const mongoclient = new MongoClient(dbUrl, dbOptions);
-let db;
-let client;
+const dbCache = {};
+const client = new MongoClient(dbUrl, dbOptions);
+const lock = new Lock();
 
+/**
+ * YOU MUST CALL "Mongo.connect()" before using anything that involves using Mongodb
+ * Mongo only needs to have 1 connection open and mutex is used to enforce that.
+ * Lock is locked at the beginning and will block any and all operations from trying to access the Mongo connection
+ * before the connection is established. Only way to unlock it is by calling "Mongo.connect()"
+ */
 class Mongo
 {
     static getClient()
     {
         return client;
     }
-    static getDB()
-    {
-        return db;
-    }
-    static getMongoClient()
-    {
-        return mongoclient;
-    }
 
-    static async connect(databaseName = dbName)
+    static async disconnect()
     {
-        if (!db && !client)
+        if (await Mongo.isConnected())
         {
-            client = await mongoclient.connect();
-            db = client.db(databaseName);
+            client.close();
         }
+    }
 
-        return db;
+    static async isConnected()
+    {
+        await lock.acquire();
+        const connected = client?.topology && client.topology.isConnected();
+        return connected;
+    }
+
+    static async connect()
+    {
+        if (!client?.topology || !client?.topology.isConnected())
+        {
+            await client.connect();
+            lock.release();
+        }
+    }
+
+    static async getDB(databaseName = dbName)
+    {
+        // this will only allow the process to continue when the mongo client makes an actual connection.
+        await lock.acquire();
+        if (!(databaseName in dbCache))
+        {
+            dbCache[databaseName] = await client.db(databaseName);
+        }
+        return dbCache[databaseName];
     }
 
     static async upsert(collection, filter, data)
     {
-        const db = await Mongo.connect();
-
+        const db = await Mongo.getDB();
         await db.collection(collection).updateOne(filter, { $set: data }, { upsert: true });
     }
 
     static async query(collection, filter, projections = {})
     {
         Object.assign(projections, { _id: 0 });
-
-        const db = await Mongo.connect();
-
+        const db = await Mongo.getDB();
         const res = await db.collection(collection).findOne(filter, { projection: projections });
-
         return res;
     }
 
     static async queryAll(collection, filter, projections)
     {
-        const db = await Mongo.connect();
-
+        const db = await Mongo.getDB();
         const res = await db.collection(collection).find(filter).project(projections).toArray();
-
         return res;
     }
 
@@ -71,17 +88,14 @@ class Mongo
         if (typeof query === 'string')
             query = { name: query };
 
-        const db = await Mongo.connect();
-
+        const db = await Mongo.getDB();
         const res = await db.collection('secrets').findOne(query, { projection: { _id: 0 } });
-
         return res;
     }
 
     static async updateSecret(key, data)
     {
-        const db = await Mongo.connect();
-
+        const db = await Mongo.getDB();
         await db.collection('secrets').updateOne({ 'name': key }, { $set: data }, { upsert: true });
     }
 }
