5d9dd18eb0cb014ca0982caf8e6e2e4098f331f2
waltermitch <devmitch256@gmail.com>
2021-10-07 09:10:36 -0700

    moved edi auth middleware to edi route only

--- src/Routes/EDI.js
+++ src/Routes/EDI.js
@@ -2,12 +2,14 @@ const controller = require('../HttpControllers/EDIController');
 const { middlewareEDI } = require('../Authorization/Auth');
 const router = require('express').Router();
 
+const prefix = '/edi';
+
 router
-    .use(middlewareEDI())
-    .get('/edi/990/outbound/reject', (req, res, next) => controller.constructor.loadTenderResponse('reject', req, res, next))
-    .get('/edi/990/outbound/accept', (req, res, next) => controller.constructor.loadTenderResponse('accept', req, res, next))
-    .post('/edi/204/inbound', controller.constructor.createTender)
-    .get('/edi/214/outbound', controller.constructor.outbound214)
-    .put('/edi/214/inbound', controller.constructor.inbound214);
+    .use(prefix, middlewareEDI())
+    .get(`${prefix}/990/outbound/reject`, (req, res, next) => controller.constructor.loadTenderResponse('reject', req, res, next))
+    .get(`${prefix}/990/outbound/accept`, (req, res, next) => controller.constructor.loadTenderResponse('accept', req, res, next))
+    .post(`${prefix}/204/inbound`, controller.constructor.createTender)
+    .get(`${prefix}/214/outbound`, controller.constructor.outbound214)
+    .put(`${prefix}/214/inbound`, controller.constructor.inbound214);
 
 module.exports = router;
