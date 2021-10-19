ba960f1d01db4c731eb1e4ff285b025f11c57fe5
waltermitch <devmitch256@gmail.com>
2021-10-19 14:16:07 -0700

    [TBE-94] added exportBills method

--- src/HttpControllers/BillController.js
+++ src/HttpControllers/BillController.js
@@ -10,18 +10,28 @@ class BillController
         res.json(result);
     }
 
-    static async createBills(req, res)
+    static async exportBill(req, res)
     {
-        const jobs = req.body?.jobs;
+        const orders = [req.params.orderGuid];
 
-        if (!jobs || jobs == 0)
+        const result = await BillService.exportBills(orders);
+
+        res.status(200);
+        res.json(result);
+    }
+
+    static async exportBills(req, res)
+    {
+        const orders = req.body?.orders;
+
+        if (!orders || orders == 0)
         {
             res.status(400);
             res.json({ 'error': 'Missing Or Empty Array' });
         }
         else
         {
-            const result = await BillService.createBills(jobs);
+            const result = await BillService.exportBills(orders);
 
             res.status(200);
             res.json(result);
