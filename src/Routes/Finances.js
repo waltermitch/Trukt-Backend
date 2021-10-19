afa509a4fe7d16a6c4f41ed7260e2bf07717fefc
waltermitch <devmitch256@gmail.com>
2021-10-19 14:15:56 -0700

    [TBE-94] added Finances routes

---
+++ src/Routes/Finances.js
@@ -0,0 +1,10 @@
+const invoiceController = require('../HttpControllers/InvoiceController.js');
+const billController = require('../HttpControllers/BillController.js');
+const { uuidRegexStr } = require('../Utils/Regexes');
+const router = require('express').Router();
+
+router
+    .get(`/invoice/order/orderGuid(${uuidRegexStr})/export`, (req, res) => http(req, res, invoiceController.exportInvoice))
+    .get(`/bill/order/orderGuid(${uuidRegexStr})/export`, (req, res) => http(req, res, billController.exportBill));
+
+module.exports = router;
