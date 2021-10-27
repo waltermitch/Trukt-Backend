391c7dca657dda9d3f4e5c7f8c19a2fd053f2310
waltermitch <devmitch256@gmail.com>
2021-10-26 20:14:54 -0700

    [TBE-94] added app logic to build required invoice payload Coupa

--- src/Services/CoupaService.js
+++ src/Services/CoupaService.js
@@ -2,9 +2,22 @@ const Coupa = require('../Coupa/API');
 
 class CoupaService
 {
-    static async createInvoices(arr)
+    static async createInvoices(invoices)
     {
+        const batch = [];
+        for (const invoice of invoices)
+            if (invoice.lines[0].commodity.identifier && invoice.externalSourceData?.coupa?.invoiced != true)
+            {
+                invoice.orderNumber = invoice.number;
+                invoice.description = invoice.lines[0].commodity.description;
+                invoice.vin = invoice.lines[0].commodity.identifier;
 
+                batch.push(invoice);
+            }
+
+        const response = await Coupa.sendInvoices(batch);
+
+        return response;
     }
 }
 
