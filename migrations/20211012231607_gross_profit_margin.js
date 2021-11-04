5a81cfed2179df161093d80027c2bb53acb766f6
waltermitch <devmitch256@gmail.com>
2021-11-03 22:20:19 -0700

    [TBE-51] removed unsigned tag from income columns

--- migrations/20211012231607_gross_profit_margin.js
+++ migrations/20211012231607_gross_profit_margin.js
@@ -15,18 +15,18 @@ exports.up = function(knex)
         {
             table.dropColumn('actual_income');
             table.dropColumn('estimated_income');
-            table.decimal('estimated_revenue', 15, 2).alter().unsigned().default(0).comment('This is the estimated amount of money a client would be paying for the order');
-            table.decimal('estimated_expense', 15, 2).alter().unsigned().default(0).comment('This is the estimated amount of money it will cost to hire a vendor/carrier to complete the order');
-            table.decimal('actual_revenue', 15, 2).alter().unsigned().default(0).comment('This is the actual amount of money that the order brings into the company');
-            table.decimal('actual_expense', 15, 2).alter().unsigned().default(0).comment('This is the actual amoutn of money that was spent on this order');
-        }).table(jobTable, (table) =>
+            table.decimal('estimated_revenue', 15, 2).alter().default(0).comment('This is the estimated amount of money a client would be paying for the order');
+            table.decimal('estimated_expense', 15, 2).alter().default(0).comment('This is the estimated amount of money it will cost to hire a vendor/carrier to complete the order');
+            table.decimal('actual_revenue', 15, 2).alter().default(0).comment('This is the actual amount of money that the order brings into the company');
+            table.decimal('actual_expense', 15, 2).alter().default(0).comment('This is the actual amount of money that was spent on this order');
+        }).alterTable(jobTable, (table) =>
         {
             table.dropColumn('actual_income');
             table.dropColumn('estimated_income');
-            table.decimal('estimated_expense', 15, 2).alter().unsigned().default(0).comment('This is the estimated amount of money it will cost to hire a vendor/carrier to complete the order');
-            table.decimal('estimated_revenue', 15, 2).alter().unsigned().default(0).comment('This is the estimated amount of money a client would be paying for the order');
-            table.decimal('actual_revenue', 15, 2).alter().unsigned().default(0).comment('This is the actual amount of money that the job brings into the company');
-            table.decimal('actual_expense', 15, 2).alter().unsigned().default(0).comment('This is the actual amount of money that was spent on this job');
+            table.decimal('estimated_expense', 15, 2).alter().default(0).comment('This is the estimated amount of money it will cost to hire a vendor/carrier to complete the job');
+            table.decimal('estimated_revenue', 15, 2).alter().default(0).comment('This is the estimated amount of money a client would be paying for the job');
+            table.decimal('actual_revenue', 15, 2).alter().default(0).comment('This is the actual amount of money that the job brings into the company');
+            table.decimal('actual_expense', 15, 2).alter().default(0).comment('This is the actual amount of money that was spent on this job');
         }).then(() =>
         {
             return knex.raw(`
@@ -66,19 +66,19 @@ exports.down = function(knex)
     {
         return knex.schema.withSchema('rcg_tms').alterTable(orderTable, (table) =>
         {
-            table.decimal('estimated_expense', 15, 2).alter().unsigned().comment('This is the estimated amount of money it will cost to hire a vendor/carrier to complete the order');
-            table.decimal('estimated_revenue', 15, 2).alter().unsigned().comment('This is the estimated amount of money a client would be paying for the order');
-            table.decimal('estimated_income', 15, 2).unsigned().comment('This is the difference between the estimated revenue and expense');
-            table.decimal('actual_revenue', 15, 2).alter().unsigned().comment('This is the actual amount of money that the order brings into the company');
-            table.decimal('actual_expense', 15, 2).alter().unsigned().comment('This is the actual amount of money that was spent on this order');
+            table.decimal('estimated_expense', 15, 2).alter().comment('This is the estimated amount of money it will cost to hire a vendor/carrier to complete the order');
+            table.decimal('estimated_revenue', 15, 2).alter().comment('This is the estimated amount of money a client would be paying for the order');
+            table.decimal('estimated_income', 15, 2).comment('This is the difference between the estimated revenue and expense');
+            table.decimal('actual_revenue', 15, 2).alter().comment('This is the actual amount of money that the order brings into the company');
+            table.decimal('actual_expense', 15, 2).alter().comment('This is the actual amount of money that was spent on this order');
             table.decimal('actual_income', 15, 2).comment('This the the actual income / profit made on the order');
-        }).table(jobTable, (table) =>
+        }).alterTable(jobTable, (table) =>
         {
-            table.decimal('estimated_expense', 15, 2).alter().unsigned().comment('This is the estimated amount of money it will cost to hire a vendor/carrier to complete the order');
-            table.decimal('estimated_revenue', 15, 2).alter().unsigned().comment('This is the estimated amount of money a client would be paying for the order');
-            table.decimal('estimated_income', 15, 2).unsigned().comment('This is the difference between the estimated revenue and expense');
-            table.decimal('actual_revenue', 15, 2).alter().unsigned().comment('This is the actual amount of money that the job brings into the company');
-            table.decimal('actual_expense', 15, 2).alter().unsigned().comment('This is the actual amount of money that was spent on this job');
+            table.decimal('estimated_expense', 15, 2).alter().comment('This is the estimated amount of money it will cost to hire a vendor/carrier to complete the order');
+            table.decimal('estimated_revenue', 15, 2).alter().comment('This is the estimated amount of money a client would be paying for the order');
+            table.decimal('estimated_income', 15, 2).comment('This is the difference between the estimated revenue and expense');
+            table.decimal('actual_revenue', 15, 2).alter().comment('This is the actual amount of money that the job brings into the company');
+            table.decimal('actual_expense', 15, 2).alter().comment('This is the actual amount of money that was spent on this job');
             table.decimal('actual_income', 15, 2).comment('This the the actual income / profit made on the job');
         });
     });
