0fc59214f87d7bf979638d93cbb7f5aedef4614c
waltermitch <devmitch256@gmail.com>
2021-11-16 02:39:45 -0800

    [TBE-112] updated seed file to reuse term id

--- seeds/devData/payment_terms.js
+++ seeds/devData/payment_terms.js
@@ -1,13 +1,13 @@
 // new Items
 const items = [
-    { name: 'Same Day Pay (5%)' },
-    { name: '2-Day QuickPay (2%)' },
-    { name: '2-Day QuickPay (NC)' },
-    { name: 'Net 15' },
-    { name: 'Net 30' }
+    { name: 'Same Day Pay (5%)', id: 1 },
+    { name: '2-Day QuickPay (2%)', id: 2 },
+    { name: '2-Day QuickPay (NC)', id: 3 },
+    { name: 'Net 15', id: 4 },
+    { name: 'Net 30', id: 5 }
 ];
 
 exports.seed = async function (knex)
 {
-    return knex('rcg_tms.invoice_bill_payment_terms').insert(items).onConflict(['name']).ignore();
+    return knex('rcg_tms.invoice_bill_payment_terms').insert(items).onConflict(['id']).merge();
 };
