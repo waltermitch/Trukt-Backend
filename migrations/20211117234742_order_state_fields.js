348e135450d4604e4558d78f6a37bbbea58c767f
waltermitch <devmitch256@gmail.com>
2021-11-18 11:31:27 -0800

    [TBE-252] Added reverse oporation of dropped columns

--- migrations/20211117234742_order_state_fields.js
+++ migrations/20211117234742_order_state_fields.js
@@ -5,10 +5,10 @@ exports.up = function (knex)
         ALTER TABLE rcg_tms.orders
         DROP COLUMN IF EXISTS is_started,
         DROP COLUMN IF EXISTS is_completed,
-        ADD COLUMN is_ready boolean NOT NULL DEFAULT FALSE,
-        ADD COLUMN is_on_hold boolean NOT NULL DEFAULT FALSE,
-        ADD COLUMN is_canceled boolean NOT NULL DEFAULT FALSE,
-        ADD COLUMN is_complete boolean NOT NULL DEFAULT FALSE,
+        ADD COLUMN is_ready boolean NOT NULL DEFAULT false,
+        ADD COLUMN is_on_hold boolean NOT NULL DEFAULT false,
+        ADD COLUMN is_canceled boolean NOT NULL DEFAULT false,
+        ADD COLUMN is_complete boolean NOT NULL DEFAULT false,
         ADD CONSTRAINT "orders_tender_constraint" CHECK ((is_tender = true AND is_ready = false AND is_complete = false AND is_canceled = false AND is_on_hold = false ) OR is_tender = false),
         ADD CONSTRAINT "orders_ready_constraint" CHECK ((is_ready = true AND is_tender = false AND is_deleted = false AND is_canceled = false) OR is_ready = false),
         ADD CONSTRAINT "orders_deleted_constraint" CHECK ((is_deleted = true AND is_canceled = false AND is_complete = false) OR is_deleted = false),
@@ -18,10 +18,10 @@ exports.up = function (knex)
         ALTER TABLE rcg_tms.order_jobs
         DROP COLUMN IF EXISTS is_started,
         DROP COLUMN IF EXISTS is_completed,
-        ADD COLUMN is_ready boolean NOT NULL DEFAULT FALSE,
-        ADD COLUMN is_on_hold boolean NOT NULL DEFAULT FALSE,
-        ADD COLUMN is_canceled boolean NOT NULL DEFAULT FALSE,
-        ADD COLUMN is_complete boolean NOT NULL DEFAULT FALSE,
+        ADD COLUMN is_ready boolean NOT NULL DEFAULT false,
+        ADD COLUMN is_on_hold boolean NOT NULL DEFAULT false,
+        ADD COLUMN is_canceled boolean NOT NULL DEFAULT false,
+        ADD COLUMN is_complete boolean NOT NULL DEFAULT false,
         ADD CONSTRAINT "order_jobs_ready_constraint" CHECK ((is_ready = true AND is_deleted = false AND is_canceled = false) OR is_ready = false),
         ADD CONSTRAINT "order_jobs_deleted_constraint" CHECK ((is_deleted = true AND is_canceled = false AND is_complete = false) OR is_deleted = false),
         ADD CONSTRAINT "order_jobs_cancel_constraint" CHECK ((is_canceled = true AND is_deleted = false AND is_complete = false ) OR is_canceled = false),
@@ -33,6 +33,8 @@ exports.down = function (knex)
 {
     return knex.raw(`
         ALTER TABLE rcg_tms.orders
+        ADD COLUMN is_started boolean NOT NULL DEFAULT false,
+        ADD COLUMN is_completed boolean NOT NULL DEFAULT false,
         DROP COLUMN IF EXISTS is_on_hold,
         DROP COLUMN IF EXISTS is_ready,
         DROP COLUMN IF EXISTS is_complete,
@@ -44,6 +46,8 @@ exports.down = function (knex)
         DROP CONSTRAINT IF EXISTS "orders_complete_constraint";
 
         ALTER TABLE rcg_tms.order_jobs
+        ADD COLUMN is_started boolean NOT NULL DEFAULT false,
+        ADD COLUMN is_completed boolean NOT NULL DEFAULT false,
         DROP COLUMN IF EXISTS is_on_hold,
         DROP COLUMN IF EXISTS is_ready,
         DROP COLUMN IF EXISTS is_complete,
