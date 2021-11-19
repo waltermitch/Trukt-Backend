34addf4bc1ece0cb199307ffbd20305cd10a3d48
waltermitch <devmitch256@gmail.com>
2021-11-18 16:18:21 -0800

    [TBE-327] added max > min constraint for vehicle_weight_classes

--- migrations/20211118235942_vehicle_weight_classes.js
+++ migrations/20211118235942_vehicle_weight_classes.js
@@ -2,13 +2,14 @@ const TABLE_NAME = 'vehicle_weight_classes';
 
 exports.up = function (knex)
 {
+    // constraint max_weight must be greater than or equal to min_weight
     return knex.schema.createTable(TABLE_NAME, function (table)
     {
         table.increments('id').primary();
         table.string('class');
         table.integer('min_weight').notNullable();
         table.integer('max_weight').notNullable();
-    });
+    }).raw(`ALTER TABLE ${TABLE_NAME} ADD CONSTRAINT max_weight_greater_than_min_weight CHECK (max_weight >= min_weight)`);
 };
 
 exports.down = function (knex)
