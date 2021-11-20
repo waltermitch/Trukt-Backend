fe8c8b4094c9bac4fde8a98b1483e76fd8c5ae84
waltermitch <devmitch256@gmail.com>
2021-11-19 18:00:07 -0800

    [TBE-223] moved when telemetry starts

--- src/ErrorHandling/Insights.js
+++ src/ErrorHandling/Insights.js
@@ -9,7 +9,7 @@ appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
     .setAutoCollectConsole(true)
     .setUseDiskRetryCaching(true)
     .setSendLiveMetrics(true);
-
+appInsights.start();
 const client = appInsights.defaultClient;
 
 switch (process.env.ENV)
@@ -19,7 +19,6 @@ switch (process.env.ENV)
     case 'staging':
     case 'prod':
     case 'production':
-        appInsights.start();
         client.context.tags[appInsights.defaultClient.context.keys.cloudRole] = process.env.ENV;
         break;
     default:
