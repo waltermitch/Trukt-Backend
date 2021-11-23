3a14574ab84ea65eb066b76f921b93ce1577eb74
waltermitch <devmitch256@gmail.com>
2021-11-23 12:59:12 -0800

    [TBE-223] added descriptions to the class parameters

--- src/ErrorHandling/Exceptions/HttpError.js
+++ src/ErrorHandling/Exceptions/HttpError.js
@@ -1,5 +1,16 @@
 module.exports = class HttpError extends Error
 {
+    /**
+    * @description This class is a custom exception class meant to be used for throwing errors when http eceptions happen
+    * @param {*} message The error message sent to the user
+    * @param {*} status The status code result from the exception
+    * @param {*} params The http request query paramaters
+    * @param {*} userId The id of the user that caused the exception
+    * @param {*} body The original request body
+    * @param {*} exception The original exception that was thrown
+    * @param {*} method The http method used to cause the exception
+    * @param {*} url The url used to cause the exception
+    */
     constructor(
             message,
             status,
