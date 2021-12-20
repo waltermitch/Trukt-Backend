0b3e732488861e74cd51aa7bd7d7ed13bde4198a
waltermitch <devmitch256@gmail.com>
2021-12-20 14:17:47 -0800

    [TBE-418] added job status updated event

--- src/EventListeners/OrderJob.js
+++ src/EventListeners/OrderJob.js
@@ -5,12 +5,14 @@ const listener = new Listener();
 
 listener.on('orderjob_stop_update', () =>
 {
-    setImmediate(async () =>
+    setImmediate(async (jobGuid) =>
     {
-        const proms = await Promise.allSettled([OrderJobService.calcJobStatus()]);
+        const proms = await Promise.allSettled([OrderJobService.calcJobStatus(jobGuid)]);
 
         for (const p of proms)
             if (p.status === 'rejected')
                 console.log(p.reason?.response?.data || p.reason);
     });
 });
+
+module.exports = listener;
