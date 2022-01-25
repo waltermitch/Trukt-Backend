ba7cd9cd3fd9cbd806e72600c923ea8a73e0e426
waltermitch <devmitch256@gmail.com>
2022-01-25 11:05:02 -0500

    [TBE-534] Fix account and contact guid__c null

---
+++ migrations/20220125091500_salesforce_guid_generation_new_function.js
@@ -0,0 +1,56 @@
+
+exports.up = function (knex)
+{
+    return knex.raw(`
+        CREATE OR REPLACE FUNCTION rcg_tms.salesforce_guid_gen()
+            RETURNS trigger
+            LANGUAGE 'plpgsql'
+            COST 1
+            STABLE NOT LEAKPROOF
+        AS $BODY$
+            BEGIN
+                IF (TG_OP = 'INSERT') THEN
+                    IF (NEW.guid__c IS NULL) THEN
+                        NEW.guid__c = gen_random_uuid();
+                    END IF;
+                ELSEIF (TG_OP = 'UPDATE') THEN
+                    IF (OLD.guid__c IS NULL AND NEW.guid__c IS NOT NULL) THEN
+                        NEW.guid__c = NEW.guid__c;
+                    ELSEIF ((NEW.guid__c <> OLD.guid__c) OR (NEW.guid__c IS NULL AND OLD.guid__c IS NOT NULL)) THEN
+                        NEW.guid__c = OLD.guid__c;
+                    ELSE
+                        NEW.guid__c = gen_random_uuid();
+                    END IF;
+                END IF;
+                RETURN NEW;
+            END;
+        $BODY$;
+  `);
+};
+
+// Down operation sets salesforce_guid_gen() as it was in sf_tables_guid_trigger.js
+exports.down = function (knex)
+{
+    return knex.raw(`
+        CREATE OR REPLACE FUNCTION rcg_tms.salesforce_guid_gen()
+            RETURNS trigger
+            LANGUAGE 'plpgsql'
+            COST 1
+            STABLE NOT LEAKPROOF
+        AS $BODY$
+            BEGIN
+                IF (TG_OP = 'INSERT') THEN
+                    IF (NEW.guid__c IS NULL) THEN
+                        NEW.guid__c = gen_random_uuid();
+                    END IF;
+                ELSEIF (TG_OP = 'UPDATE') THEN
+                    -- do not allow users to change the guid__c once it is assigned
+                    IF(NEW.guid__c <> OLD.guid__c) THEN
+                        NEW.guid__c = OLD.guid__c;
+                    END IF;
+                END IF;
+                RETURN NEW;
+            END;
+        $BODY$;
+  `);
+};
