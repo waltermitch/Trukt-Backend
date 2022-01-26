5d4f0adfaf67efd6d4c1b6691454786e2553783a
waltermitch <devmitch256@gmail.com>
2022-01-25 19:32:51 -0500

    [TBE-534] Refactor new salesforce_guid_gen function

--- migrations/20220125091500_salesforce_guid_generation_new_function.js
+++ migrations/20220125091500_salesforce_guid_generation_new_function.js
@@ -2,27 +2,42 @@
 exports.up = function (knex)
 {
     return knex.raw(`
+        CREATE OR REPLACE FUNCTION rcg_tms.uuid_valid(uuid varchar)
+            returns boolean as $$
+            select uuid IS NOT NULL AND uuid ~ '^[0-9a-f]{8}-(:\\?[0-9a-f]{4}-){3}[0-9a-f]{12}$';
+        $$ language sql;
+
         CREATE OR REPLACE FUNCTION rcg_tms.salesforce_guid_gen()
-            RETURNS trigger
-            LANGUAGE 'plpgsql'
-            COST 1
-            STABLE NOT LEAKPROOF
-        AS $BODY$
-            BEGIN
-                IF (TG_OP = 'INSERT') THEN
-                    IF (NEW.guid__c IS NULL) THEN
-                        NEW.guid__c = gen_random_uuid();
-                    END IF;
-                ELSEIF (TG_OP = 'UPDATE') THEN
-                    IF (OLD.guid__c IS NOT NULL) THEN
-                        NEW.guid__c = OLD.guid__c;
-                    ELSE
-                        NEW.guid__c = gen_random_uuid();
-                    END IF;
+            returns trigger 
+            LANGUAGE plpgsql
+            VOLATILE NOT LEAKPROOF
+            AS $function$
+        DECLARE
+            is_old_uuid_valid boolean;
+            is_new_uuid_valid boolean;
+        BEGIN
+            IF (TG_OP = 'INSERT') THEN
+                select rcg_tms.uuid_valid(NEW.guid__c) INTO is_new_uuid_valid;
+
+                IF (is_new_uuid_valid IS false) THEN
+                    NEW.guid__c = gen_random_uuid();
                 END IF;
-                RETURN NEW;
-            END;
-        $BODY$;
+            ELSEIF (TG_OP = 'UPDATE') THEN  
+                select rcg_tms.uuid_valid(OLD.guid__c) INTO is_old_uuid_valid;
+
+                IF (is_old_uuid_valid is true) THEN
+                    NEW.guid__c = OLD.guid__c;
+                ELSE
+                    select rcg_tms.uuid_valid(NEW.guid__c) INTO is_new_uuid_valid;
+
+                    IF (is_new_uuid_valid is false) THEN
+                        NEW.guid__c = gen_random_uuid();
+                    END IF;
+                END IF;                
+            END IF;
+        RETURN NEW;
+        END;
+        $function$
   `);
 };
 
@@ -30,6 +45,8 @@ exports.up = function (knex)
 exports.down = function (knex)
 {
     return knex.raw(`
+        DROP FUNCTION IF EXISTS rcg_tms.uuid_valid;
+
         CREATE OR REPLACE FUNCTION rcg_tms.salesforce_guid_gen()
             RETURNS trigger
             LANGUAGE 'plpgsql'
