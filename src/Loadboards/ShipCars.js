42132fd9a11000e26b256dda6f6102572f148fbc
waltermitch <devmitch256@gmail.com>
2022-02-01 15:03:40 -0800

    [TBE-545] refactored to validate dispatching and handling dispatch

--- src/Loadboards/ShipCars.js
+++ src/Loadboards/ShipCars.js
@@ -70,12 +70,18 @@ class ShipCars extends Loadboard
         return payload;
     }
 
-    dispatchJSON()
+    validateDispatch(job)
     {
+        super.validateDispatch(job);
+
         if(!this.data.vendor.scId)
         {
-            throw new Error('Carrier missing ShipCars Id');
+            throw new Error('Please tell the carrier to register with ShipCars before dispatching to them.');
         }
+    }
+
+    dispatchJSON()
+    {
         const payload = {
             'carrier': this.data.vendor.scId,
 
@@ -298,26 +304,25 @@ class ShipCars extends Loadboard
         try
         {
             const dispatch = OrderJobDispatch.fromJson(payloadMetadata.dispatch);
-            dispatch.externalGuid = response.dispatchRes.id;
-            dispatch.setUpdatedBy(dispatch.createdByGuid);
-
-            allPromises.push(OrderJobDispatch.query(trx).patch(dispatch).findById(dispatch.guid));
             const objectionPost = LoadboardPost.fromJson(payloadMetadata.post);
+            
             if (response.hasErrors)
             {
-                objectionPost.isSynced = false;
-                objectionPost.isPosted = false;
+                objectionPost.isSynced = true;
                 objectionPost.hasError = true;
                 objectionPost.apiError = response.errors;
+
+                dispatch.setToError(response.errors);
+                dispatch.setUpdatedBy(dispatch.createdByGuid);
+                allPromises.push(Job.query(trx).patch({ status: Job.STATUS.READY }).findById(objectionPost.jobGuid));
             }
             else
             {
-                objectionPost.externalPostGuid = null;
-                objectionPost.status = 'unposted';
-                objectionPost.isCreated = true;
-                objectionPost.isSynced = true;
-                objectionPost.isPosted = false;
+                objectionPost.setToUnposted();
                 objectionPost.externalGuid = response.order.id;
+
+                dispatch.externalGuid = response.dispatchRes.id;
+                dispatch.setUpdatedBy(dispatch.createdByGuid);
 
                 const job = await Job.query(trx).findById(objectionPost.jobGuid).withGraphFetched('[ commodities(distinct, isNotDeleted).[vehicle]]');
                 const commodityPromises = this.updateCommodity(job.commodities, response.dispatchRes.vehicles);
@@ -332,11 +337,11 @@ class ShipCars extends Loadboard
                     .leftJoin('salesforce.contacts', 'salesforce.accounts.sfId', 'salesforce.contacts.accountId')
                     .where({ 'salesforce.contacts.guid': dispatch.vendorAgentGuid || dispatch.vendorAgent.guid })
                     .select('salesforce.accounts.name as vendorName',
-                        'salesforce.accounts.guid as vendorGuid',
-                        'salesforce.accounts.dot_number as dotNumber',
-                        'salesforce.contacts.guid as agentGuid',
-                        'salesforce.contacts.name as agentName');
-
+                    'salesforce.accounts.guid as vendorGuid',
+                    'salesforce.accounts.dot_number as dotNumber',
+                    'salesforce.contacts.guid as agentGuid',
+                    'salesforce.contacts.name as agentName');
+                
                 await StatusManagerHandler.registerStatus({
                     orderGuid: job.orderGuid,
                     userGuid: dispatch.createdByGuid,
@@ -353,6 +358,7 @@ class ShipCars extends Loadboard
                     }
                 });
             }
+            allPromises.push(OrderJobDispatch.query(trx).patch(dispatch).findById(dispatch.guid));
             objectionPost.setUpdatedBy(process.env.SYSTEM_USER);
             allPromises.push(LoadboardPost.query(trx).patch(objectionPost).findById(objectionPost.guid));
             await Promise.all(allPromises);
