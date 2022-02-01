f2977383c33d16ab81716c5a233c22a9101b3ec2
waltermitch <devmitch256@gmail.com>
2022-02-01 12:58:57 -0800

    [TBE-579] fixed knex down command

--- migrations/20220201204900_copart_photos_index.js
+++ migrations/20220201204900_copart_photos_index.js
@@ -1,19 +1,16 @@
 const SCHEMA_NAME = 'copart';
 const TABLE_NAME = 'photos';
-const INDEX_NAME = 'photos_lot_index';
+const INDEX_NAME = 'lot_index';
 
 exports.up = function (knex)
 {
     return knex.schema.withSchema(SCHEMA_NAME).table(TABLE_NAME, function (table)
     {
-        table.index(['lot'], INDEX_NAME);
+        table.index(['lot'], `${TABLE_NAME}_${INDEX_NAME}`);
     });
 };
 
 exports.down = function (knex)
 {
-    return knex.schema.withSchema(SCHEMA_NAME).table(TABLE_NAME, function (table)
-    {
-        table.dropIndex(INDEX_NAME);
-    });
+    return knex.raw(`DROP INDEX IF EXISTS ${SCHEMA_NAME}.${TABLE_NAME}_${INDEX_NAME}`);
 };
