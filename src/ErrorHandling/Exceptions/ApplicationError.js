34c5a23b640067bc5f62e19c6c5c498e21b9b0f1
waltermitch <devmitch256@gmail.com>
2022-02-11 15:25:52 -0400

    [TBE-564] Added third param of optional info to ApplicationError

--- src/ErrorHandling/Exceptions/ApplicationError.js
+++ src/ErrorHandling/Exceptions/ApplicationError.js
@@ -16,15 +16,22 @@ class ApplicationError extends Error
     status;
 
     /**
+     * @type {Record<string, unknown>} - Helpful data of the exception.
+     */
+    helperInfo;
+
+    /**
      * @param {string} message - The message of the exception.
      * @param {number} status - The http status code of the exception.
+     * @param {Record<string, unknown>} helperInfo - Helpful data of the exception.
      */
-    constructor(message = '', status = 500)
+    constructor(message = '', status = 500, helperInfo = {})
     {
         super(message);
         this.name = this.constructor.name;
         this.message = message;
         this.status = status;
+        this.helperInfo = helperInfo;
     }
 
     toJSON()
@@ -32,7 +39,8 @@ class ApplicationError extends Error
         return {
             errorType: this.name,
             message: this.message,
-            status: this.status
+            status: this.status,
+            ...this.helperInfo
         };
     }
 }
