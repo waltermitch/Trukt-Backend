c57029c737947c5ee81089a1ad2bbd5bd60c4a11
waltermitch <devmitch256@gmail.com>
2022-02-11 19:38:50 -0800

    [TBE-518] added Agistix EDI 214 code notifications

--- src/EventListeners/EDI.js
+++ src/EventListeners/EDI.js
@@ -4,7 +4,10 @@ const EDIService = require('../Services/EDIService');
 /**
  * For descriptions of the Events please see the Confluence page "Event System"
  */
-emitter.on('order_stop_delivery_started', ({ order, job, stop, datetime }) => { });
+emitter.on('order_stop_delivery_started', ({ order, job, stop, datetime }) =>
+{
+    setImmediate(async () => { EDIService.sendCode({ order, job, stop, datetime }, 'X1'); });
+});
 emitter.on('order_stop_delivery_unstarted', ({ order, job, stop, datetime }) => { });
 emitter.on('order_stop_delivery_completed', ({ order, job, stop, datetime }) =>
 {
@@ -30,9 +33,15 @@ emitter.on('order_stop_delivery_scheduled_late', ({ order, job, stop, datetime }
 });
 emitter.on('order_stop_delivery_scheduled_early', ({ order, job, stop, datetime }) => { });
 
-emitter.on('order_stop_pickup_started', ({ order, job, stop, datetime }) => { });
+emitter.on('order_stop_pickup_started', ({ order, job, stop, datetime }) =>
+{
+    setImmediate(async () => { EDIService.sendCode({ order, job, stop, datetime }, 'X3'); });
+});
 emitter.on('order_stop_pickup_unstarted', ({ order, job, stop, datetime }) => { });
-emitter.on('order_stop_pickup_completed', ({ order, job, stop, datetime }) => { });
+emitter.on('order_stop_pickup_completed', ({ order, job, stop, datetime }) =>
+{
+    setImmediate(async () => { EDIService.sendCode({ order, job, stop, datetime }, 'AF'); });
+});
 emitter.on('order_stop_pickup_uncompleted', ({ order, job, stop, datetime }) => { });
 emitter.on('order_stop_pickup_scheduled', ({ order, job, stop, datetime }) => { });
 emitter.on('order_stop_pickup_unscheduled', ({ order, job, stop, datetime }) => { });
