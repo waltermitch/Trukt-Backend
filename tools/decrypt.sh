1e20c0439303f405fd4d977a9977d92e7e336c1a
waltermitch <devmitch256@gmail.com>
2022-02-22 22:13:23 -0800

    [TBE-640] added some logging to see decryption flow

--- tools/decrypt.sh
+++ tools/decrypt.sh
@@ -13,4 +13,5 @@ if [[ -z $ENC_SALT ]] || [[ -z $ENC_KEY ]] || [[ -z $ENC_IV ]]; then
     exit 1
 else
     openssl enc -d -aes-256-cbc -md sha512 -S $ENC_SALT -K $ENC_KEY -iv $ENC_IV -in $1 -out $2
+    echo "Decrypted $1 to $2"
 fi
