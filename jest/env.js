8e82f66acd04378ef0d0f3d423dc05d186999b16
waltermitch <devmitch256@gmail.com>
2022-02-22 21:02:28 -0800

    [TBE-640] updated jest pre-test script to do nothing

--- jest/env.js
+++ jest/env.js
@@ -1,6 +1,1 @@
-const config = require('../local.settings.json').Values;
-
-const arr = Array.from(Object.keys(config));
-
-for (let i = 0; i < arr.length; i++)
-    process.env[`${[arr[i]]}`] = config[`${[arr[i]]}`];
+// do nothing
