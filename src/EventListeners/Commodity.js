03d5aabad7195c38dce7accb0a0888da9002d0e3
waltermitch <devmitch256@gmail.com>
2022-03-03 17:57:05 -0800

    [TBE-127] corrected typo

--- src/EventListeners/Commodity.js
+++ src/EventListeners/Commodity.js
@@ -1,10 +1,10 @@
 const listener = require('./index');
-const CommoditySerivce = require('../Services/CommodityService');
+const CommodityService = require('../Services/CommodityService');
 
 listener.on('commodity_deleted', ({ orderGuid, jobGuid, commodities, currentUser }) =>
 {
     setImmediate(async () =>
     {
-        const proms = await Promise.allSettled([CommoditySerivce.registerDeletedCommodities(orderGuid, jobGuid, commodities, currentUser)]);
+        const proms = await Promise.allSettled([CommodityService.registerDeletedCommodities(orderGuid, jobGuid, commodities, currentUser)]);
     });
 });
