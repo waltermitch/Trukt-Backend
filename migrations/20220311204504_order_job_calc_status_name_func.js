4a6b0f69b5cd46dafd743bae3c3fe9fb8b2d443b
waltermitch <devmitch256@gmail.com>
2022-03-14 14:54:15 -0400

    [TBE-694] WIP: added new state validation
    For both transport and any other job

--- migrations/20220311204504_order_job_calc_status_name_func.js
+++ migrations/20220311204504_order_job_calc_status_name_func.js
@@ -16,7 +16,7 @@ exports.up = function(knex)
             job record;
             loadboard_request_guid uuid;
             is_valid_stop boolean default false;
-
+            valid_new_job_count integer;
         begin
             select
             *
@@ -30,35 +30,146 @@ exports.up = function(knex)
         inner join rcg_tms.loadboard_requests lr 
         on lp.guid = lr.loadboard_post_guid where lp.job_guid = param_job_guid;
 
-        if job.is_ready = false 
+        -- evaluate new state
+        if job.is_ready = false
             and job.vendor_guid is null
             and job.vendor_contact_guid is null
-            and job.vendor_agent_guid is null 
+            and job.vendor_agent_guid is null
             and loadboard_request_guid is null
-            and job.is_on_hold = false 
-            and job.is_deleted = false 
-            and job.is_canceled = false 
+            and job.is_on_hold = false
+            and job.is_deleted = false
+            and job.is_canceled = false
             and job.is_complete = false 
         then
+            -- new state for transport jobs
             if job.type_id = 1 then 
-                select count(*) > 1, count(comm.guid) into is_valid_stop from rcg_tms.order_stop_links osl 
-                inner join rcg_tms.order_stops os 
-                on osl.stop_guid = os.guid 
-                inner join rcg_tms.commodities comm
-                on osl.commodity_guid = comm.guid
-                where osl.job_guid = param_job_guid and os.date_requested_start is not null
-                group by osl.job_guid
-                having count(comm.guid) > 1;
+                select count(*)
+                into 
+                    valid_new_job_count  from 
+                (select
+                    (
+                    select
+                        count(*) > 0
+                    from
+                        rcg_tms.loadboard_requests lbr
+                    left join rcg_tms.loadboard_posts lbp2 on
+                        lbp2.guid = lbr.loadboard_post_guid
+                    where
+                        lbr.is_valid
+                        and lbr.is_accepted
+                        and lbp2.job_guid = oj.guid
+                    ) as has_accepted_requests,
+                    stop.pickup_requested_date,
+                    stop.delivery_requested_date,
+                    stop.pickup_sequence,
+                    stop.delivery_sequence,
+                    stop.bad_pickup_address,
+                    stop.bad_delivery_address,
+                    stop.commodity_guid
+                from
+                    rcg_tms.order_jobs oj
+                join (
+                    select
+                    distinct
+                    os.date_requested_start pickup_requested_date,
+                    os2.date_requested_start delivery_requested_date,
+                    osl.job_guid,
+                    os."sequence" pickup_sequence,
+                    os2."sequence" delivery_sequence,
+                    osl.commodity_guid,
+                    case
+                        when t.is_resolved then null
+                        else CONCAT(t.street1, ' ', t.city, ' ', t.state, ' ', t.zip_code)
+                    end as bad_pickup_address,
+                    case
+                        when t2.is_resolved then null
+                        else CONCAT(t2.street1, ' ', t2.city, ' ', t2.state, ' ', t2.zip_code)
+                    end as bad_delivery_address
                 
+                from
+                    rcg_tms.order_stop_links osl
+                left join rcg_tms.order_stops os on
+                    osl.stop_guid = os.guid
+                left join rcg_tms.terminals t on
+                    os.terminal_guid = t.guid,
+                    rcg_tms.order_stop_links osl2
+                left join rcg_tms.order_stops os2 on
+                    osl2.stop_guid = os2.guid
+                left join rcg_tms.terminals t2 on
+                    os2.terminal_guid = t2.guid
+                where
+                    os.stop_type = 'pickup'
+                    and os2.stop_type = 'delivery'
+                    and os."sequence" < os2."sequence"
+                    and osl.order_guid = osl2.order_guid
+                    and osl.job_guid = param_job_guid 
+                order by
+                    os2."sequence" desc,
+                    os."sequence" asc
+                limit 1) as stop on
+                    stop.job_guid = oj.guid
+                where
+                    guid = param_job_guid 
+                    and oj.is_transport) as valid_stops;
 
-                return is_valid_stop;
+                if valid_new_job_count > 0 then 
+                    job_status := 'new';
+                end if;
+            -- new state for any other job
+            else
+                select
+                    count(*)
+                into
+                    valid_new_job_count
+                from 
+                (select
+                    (
+                        select
+                            count(*) > 0
+                        from
+                            rcg_tms.loadboard_requests lbr
+                        left join rcg_tms.loadboard_posts lbp2 on
+                            lbp2.guid = lbr.loadboard_post_guid
+                        where
+                            lbr.is_valid
+                            and lbr.is_accepted
+                            and lbp2.job_guid = oj.guid) as has_accepted_requests,
+                    stop.commodity_guid,
+                    stop.not_resolved_address,
+                    stop.stop_type
+                from
+                    rcg_tms.order_jobs oj
+                join (
+                    select
+                        distinct
+                            osl.commodity_guid,
+                            osl.job_guid,
+                    case
+                        when t.is_resolved then null
+                        else CONCAT(t.street1, ' ', t.state, ' ', t.city, ' ', t.zip_code)
+                    end as not_resolved_address,
+                        os.stop_type
+                    from
+                        rcg_tms.order_stop_links osl
+                    left join rcg_tms.order_stops os on
+                        osl.stop_guid = os.guid
+                    left join rcg_tms.terminals t on
+                        os.terminal_guid = t.guid
+                    where
+                        osl.job_guid = param_job_guid) as stop on
+                    stop.job_guid = oj.guid
+                where
+                    guid = param_job_guid
+                    and oj.is_transport = false) as valid_stops;
+            
+                if valid_new_job_count > 0 then
+                    job_status := 'new';
+                end if;
             end if;
-            job_status := 'new';
-
         end if;
-        return job_status;
-        end;
 
+            return job_status;
+        end;
         $$;
     `);
 };
