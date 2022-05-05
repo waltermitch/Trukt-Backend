ad064c0a077896a75fbf0b583d1dceef3891e76f
waltermitch <devmitch256@gmail.com>
2022-05-05 09:38:36 -0700

    [TBE-1967] removed || null

--- src/Services/TerminalService.js
+++ src/Services/TerminalService.js
@@ -31,7 +31,7 @@ class TerminalService
     {
         const [res] = await Trimble.geocodeAddress(address, { format: true });
 
-        return res || null;
+        return res;
     }
 
     static async getById(terminalId)
