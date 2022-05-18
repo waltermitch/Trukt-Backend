f5350c9d594e21e5c07cd060e9cd5496eeb0a4df
waltermitch <devmitch256@gmail.com>
2022-05-18 16:51:33 -0500

    [TBE-1975] Added CaseLabel Model

--- src/Models/CaseLabel.js
+++ src/Models/CaseLabel.js
@@ -3,7 +3,29 @@ const BaseModel = require("./BaseModel");
 
 class CaseLabel extends BaseModel
 {
+    static get tableName()
+    {
+        return 'rcgTms.caseLabels';
+    }
 
+    static get idColumn()
+    {
+        return 'id';
+    }
+    static get relationMappings()
+    {
+        const relations = {
+            case: {
+                relation: BaseModel.HasOneRelation,
+                modelClass: require('./Case'),
+                join: {
+                    from: 'rcgTms.cases.caseLabelId',
+                    to: 'rcgTms.caseLabels.id'
+                }
+            }
+        }
+        return relations;
+    }
 }
 
 Object.assign(CaseLabel.prototype, RecordAuthorMixin);
