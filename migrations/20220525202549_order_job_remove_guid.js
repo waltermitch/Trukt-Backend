f0e97ec5ec048515df1478363937d7a631ea6a6a
waltermitch <devmitch256@gmail.com>
2022-05-25 17:16:31 -0500

    [TBE-1978] Updated migration

--- migrations/20220525202549_order_job_remove_guid.js
+++ migrations/20220525202549_order_job_remove_guid.js
@@ -1,17 +1,9 @@
 const TABLE_NAME = 'order_job_cases';
-const SCHEMA_NAME = 'rcg_tms';
-const FUNCTION_NAME = 'rcg_gen_uuid';
 exports.up = function(knex)
 {
-    return knex.schema.withSchema(SCHEMA_NAME)
-        .alterTable(TABLE_NAME, (table) =>
-        {
-        })
-        .raw(`DROP TRIGGER IF EXISTS rcg_${TABLE_NAME}_guid ON rcg_tms.${TABLE_NAME};`);
+    return knex.raw(`DROP TRIGGER IF EXISTS rcg_${TABLE_NAME}_guid ON rcg_tms.${TABLE_NAME};`);
 };
 
 exports.down = function(knex)
 {
-    return knex.schema.withSchema('rcg_tms')
-    .dropTableIfExists(TABLE_NAME);
 };
