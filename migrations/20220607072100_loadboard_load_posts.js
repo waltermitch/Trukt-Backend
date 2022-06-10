84d01182093c45962459895445cce621e8ad1de2
waltermitch <devmitch256@gmail.com>
2022-06-10 12:07:55 -0700

    [TBE-2162] resolved github comments

--- migrations/20220607072100_loadboard_load_posts.js
+++ migrations/20220607072100_loadboard_load_posts.js
@@ -8,7 +8,7 @@ exports.up = function(knex) {
         table.uuid('guid').primary().unique().notNullable();
         const jobguid = 'job_guid';
         table.uuid(jobguid).notNullable();
-        table.foreign(jobguid).references('guid').inTable('rcg_tms.order_jobs').onDelete('SET NULL');
+        table.foreign(jobguid).references('guid').inTable('rcg_tms.order_jobs').onDelete('CASCADE');
         table.string('job_category',16).nullable();
         table.string('job_type',24).nullable();
         table.string('job_number',24).nullable();
@@ -22,36 +22,34 @@ exports.up = function(knex) {
         table.foreign(dispatcherguid).references('guid').inTable('rcg_tms.tms_users');
         table.text('instructions').nullable();
         table.string('load_type').nullable();
-        table.string('carrier_pay').nullable();
-        table.decimal('distance',14,2);
-        table.decimal('rate_per_mile',14,2);
+        table.numeric('carrier_pay',15,2).nullable();
+        table.numeric('distance',8,1);
+        table.numeric('rate_per_mile',15,2);
         table.specificType('equipment', 'text ARRAY');
         table.timestamp('date_created').defaultTo(knex.fn.now());
         table.timestamp('pickup_date_requested_end');
         table.timestamp('pickup_date_requested_start');
-        table.timestamp('pickup_date_requested_type');
+        table.datetime('pickup_date_requested_type');
+
         table.string('pickup_street1',64).nullable().index();
         table.string('pickup_street2',64).nullable();
         table.string('pickup_state',100).nullable().index();
         table.string('pickup_city',64).nullable().index();
         table.string('pickup_country',64).nullable().index();
         table.string('pickup_zip_code',16).notNullable().index();
-        table.float('pickup_latitude',14,10).nullable();
-        table.float('pickup_longitude',14,10).nullable();
+        table.float('pickup_latitude',15,7).nullable();
+        table.float('pickup_longitude',15,7).nullable();
         table.timestamp('delivery_date_requested_end').nullable();
         table.timestamp('delivery_date_requested_start').nullable();
         
         let exists = false
         table.enu(`delivery_date_requested_type`, [
-            'estimated',
-            'exactly',
-            'no later than',
-            'no earlier than'
+            'date_schedule_types'
         ], {
             useNative: true, enumName: 'delivery_date_requested_type', existingType: exists
         });
         exists = true
-        table.string('delivery_street1',64).nullable();
+        table.string('delivery_street1',64).nullable().index();
         table.string('delivery_street2',64).nullable();
         table.string('delivery_state',100).nullable().index();
         table.string('delivery_city',64).nullable().index();
@@ -60,15 +58,15 @@ exports.up = function(knex) {
         table.float('delivery_latitude',15,7).nullable();
         table.float('delivery_longitude',15,7).nullable();
         table.string('commodity_capacity').nullable();
-        table.specificType('commodity_type', 'text ARRAY').nullable();
+        table.specificType('commodity_type', 'text ARRAY').nullable().index();
         table.specificType('commodity_category', 'text ARRAY').nullable().index();
-        table.enu('commodity_damaged', ternary_options, { useNative: true, enumName: 'ternary_type' }).defaultTo('unknown').notNullable().index();
-        table.text('commodity_description',255).nullable();
-        table.enu('commodity_inoperable', null, { useNative: true, enumName: 'ternary_type', existingType: true }).defaultTo('unknown').notNullable().index();
-        table.decimal('commodity_length').nullable();
-        table.decimal('commodity_quantity').nullable();
-        table.decimal('commodity_weight').nullable();
-
+        table.enu('commodity_damaged', ternary_options, { useNative: true, enumName: 'ternary_types',existingType: true }).defaultTo('unknown').notNullable().index();
+        table.text('commodity_description').nullable();
+        table.enu('commodity_inoperable', null, { useNative: true, enumName: 'ternary_types', existingType: true }).defaultTo('unknown').notNullable().index();
+        table.integer('commodity_length').nullable();
+        table.integer('commodity_quantity').nullable();
+        table.integer('commodity_weight').nullable();
+        table.uuid('guid').defaultTo(knex.raw('gen_random_uuid()'));
         
     })
 };
